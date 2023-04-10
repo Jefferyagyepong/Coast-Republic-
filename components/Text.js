e328b7f82c333b62d1a5b27f11d2cad40c6110ed
Jefferyagyepong <agyepong9jeff@icloud.com>
2023-04-10 02:26:19 +0000

    burger changed

--- components/Text.js
+++ components/Text.js
@@ -5,24 +5,12 @@ export default function Text (){
     return (
      
 
-        <div className="text">
-           <div className="words words-1">
-                <span>P</span>
-                <span>r</span>
-                  <span>e</span>
-            </div>
-             <div className="words words-1">
-                <span>O</span>
-                <span>r</span>
-                <span>d</span>
-                <span>e</span>
-                <span>r</span>
-                
-            </div>
-              <div className="words words-1">
-                <span>N</span>
-                <span>O</span>
-                  <span>W</span>
+        <div className="wrapper-one">
+           <div className="pre-main">
+              
+          <div className="pre">Pre</div>
+          <div className="order">Order</div>
+             <div className="now">Now</div>
             </div>
        
       </div>
