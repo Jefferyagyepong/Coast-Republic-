a02460f8cf6a02f98ff39504bbfeabd36cc4f2a4
Jefferyagyepong <agyepong9jeff@icloud.com>
2023-10-15 13:35:22 +0000

    uppdates

--- pages/api/contactpost.js
+++ pages/api/contactpost.js
@@ -3,10 +3,7 @@ export default function handler(req, res) {
   const body = req.body;
 
   res.status(200).json({
-    user: `First Name: ${body.firstName}
-      Last Name: ${body.lastName} 
-      Region: ${body.region} 
-      Message: ${body.message}`,
+    user: `First Name: ${body.firstName} Last Name: ${body.lastName} Region: ${body.region}  Message: ${body.message}`,
   });
   if (req.method === "GET") {
     res.status(200).json();
