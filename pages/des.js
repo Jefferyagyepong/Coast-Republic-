b33e88ad4892f8e7149aec7cc209e9a0f7a513f6
Jefferyagyepong <agyepong9jeff@icloud.com>
2023-10-19 02:04:22 +0000

    init

---
+++ pages/des.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Check from "../components/Check";
+import Footer from "../components/Footer";
+import DesCard from ".@/components/Descard";
+import Search from "../components/Search";
+import Header from "../components/Header";
+import styles from "../sass/components/ShopPage.module.css";
+import { getProducts } from "./api/products/index";
+
+const ShopPage = ({ products }) => {
+  return (
+    <>
+      <Head>
+        <title>Search | Coast Republic</title>
+        <link rel="apple-touch-icon" href="" />
+        <meta property="og:title" content="Home | Coast Republic" />
+        <meta property="og:type" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="" />
+
+        <meta name="description" content="Coast Republic  Store" />
+        <meta
+          name="keywords"
+          content=" e-commerce, T-sirts , Ghana, Quality T-shirts, Clothing, Affordable clothing, crew neck, T-shirt print, store"
+        />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="author" content="Jeffery Agyepong" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      
+     
+            {products.map(product => (
+              <DesCard key={product.id} product={product} />
+            ))}
+      
+     
+      </main>
+    </>
+  );
+};
+
+export default ShopPage;
+
+export async function getStaticProps() {
+  const products = await getProducts();
+  return { props: { products } };
+}
