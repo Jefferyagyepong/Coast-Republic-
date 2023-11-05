b343222fb8bb6c7af558e6bb37bdc2721c1a8955
Jefferyagyepong <agyepong9jeff@icloud.com>
2023-11-05 02:54:29 +0000

    init

--- components/Head/Header.js
+++ components/Head/Header.js
@@ -47,7 +47,7 @@ export default function Header() {
             <Image src={"/crlogo.svg"} width={99} height={90} alt="logo" />
           </Link>
       
-            <SearchInput />
+        
           </div>
       
 
