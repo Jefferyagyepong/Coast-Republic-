36f7b2f1bc52b57b258245950ff965f81d8baaa1
Jefferyagyepong <agyepong9jeff@icloud.com>
2023-11-10 01:27:06 +0000

    init

--- components/Head/Top.js
+++ components/Head/Top.js
@@ -1,8 +1,9 @@
 export default function Top() {
   return (
-    <div class="fade-in-text">
+    <div className="fade-in-text">
       {" "}
       <span> Free delivery to all regions on orders over &#8373;500</span>
     </div>
+
   );
 }
