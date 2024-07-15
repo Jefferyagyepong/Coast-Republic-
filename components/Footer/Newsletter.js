31d756d28ad05a203a39348118bc30a4da416cc8
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-15 15:31:17 +0000

    Update Newsletter.js

--- components/Footer/Newsletter.js
+++ components/Footer/Newsletter.js
@@ -3,14 +3,14 @@ export default function Newsletter() {
     return (
       <div className="newsletter-box">
         <div className="newsletter-card">
-          <h3>Join our newsletter now!</h3>
+          <h3>Join our newsletter now!</h3><br/>
           <p>
             Register now and get our latest updates and promos
-          </p>
+          </p><br/>
           <form>
             <input type="email" name="email" placeholder="email" />
             <button className="button-link">Join</button>
-          </form>
+          </form><br/>
         </div>
       </div>
     );
