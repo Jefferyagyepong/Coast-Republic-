dfafd56242678f16ad2e8e55125cf011b1fec9d7
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-15 15:51:46 +0000

    Update Contact.js

--- components/Forms/Contact.js
+++ components/Forms/Contact.js
@@ -1,8 +1,8 @@
 export default function Contact() {
   return (
     <div className="terms-wrap">
-      <h4 className="footer-text">CONTACT US</h4>
-      <p >
+      <h4>CONTACT US</h4>
+      <p clqssName="delivery-text">
         IF YOU HAVE ANY QUESTIONS, PLEASE FEEL FREE TO REACH OUT VIA EMAIL. WE
         WILL GET BACK TO YOU AS SOON AS WE CAN WE WOULD ALSO LOVE TO HEAR FROM
         YOUR FEEDBACK, PLEASE LET US KNOW HOW WE CAN IMPROVE
