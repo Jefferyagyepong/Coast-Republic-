dbd36e14004a7ed81b237e8c878dd68d0d22f258
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-15 19:23:46 +0000

    Update New.js

--- components/Products/New.js
+++ components/Products/New.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function New() {
   return (
     <div className="new">
-      <h3>WHATS NEW TODAY </h3><br/>
+     <br/> <h3>WHATS NEW TODAY </h3><br/>
      
-      <span>12</span><br/>
+      <span>12</span><br/><br/>
     
       <p>Discover what just landed at Coast Republic</p>
     </div>
