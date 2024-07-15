ebf6729297cb825ca37c9f68a7501e5127424c9e
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-15 19:16:27 +0000

    Update CoastApp.js

--- components/Products/CoastApp.js
+++ components/Products/CoastApp.js
@@ -3,10 +3,10 @@ export default function IbridApp() {
     return (
       <div className="ibrid-box">
         <h3 className="ibrid-heading">Download Our App</h3>
-        <span>
+        <p>
           Unlock a world of limetless possiblities. Download the Coast Republic app and
           embark on a journey of discovery and connection.
-        </span>
+        </p>
 
         <div className="ibrid-flex">
           <Image src={"/apple.svg"} alt="apple" width={70} height={100} />
