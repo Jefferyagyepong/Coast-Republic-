ebd7c41b0bd2a03f226e3fccd8d257062f8b8521
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-10 11:58:25 +0000

    Update New.js

--- components/Products/New.js
+++ components/Products/New.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function New() {
   return (
-    <div className="new">
+    <div className="ibrid-box">
      <br/> <h3>WHATS NEW TODAY </h3><br/>
      
       <span>12</span><br/><br/>
