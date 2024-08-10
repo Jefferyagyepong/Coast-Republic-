5370fc373df23b4c0f4253c7420a31d2ea91e1d9
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-10 08:52:06 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -3,26 +3,17 @@
 export default function Sale() {
   return (
     <div className="shop-now">
-      <div className="flex-row">
-        <h1>15</h1>
-        <div className="flex-column">
-          <h4>%</h4>
-          <h4>OFF</h4>
-        </div>
-      </div>
-      <br />
-      <br />
-      <h6 >NEW SEASON</h6>
-      <br />
-      <br />
-      <h6 className="color-linear-gradiant">
+ 
+      <h6>NEW SEASON</h6>
+      
+      <h6>
         Discount automatically applied at check out
       </h6>
-      <br />
-      <br />
+      
+      
       <Link href={"/shop"} className="add-to-cart">
         Shop Now
       </Link>
-    </div>
+    
   );
 }
