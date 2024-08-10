27648703b319d77f819104bd3328cdd1338e6bb1
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-10 18:01:27 +0000

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,40 +4,30 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
+     <div className="footer-align-left" >
       <Link href={"./"}>
         <h2>Coast Republic Inc.</h2>
-        <br />
       </Link>
       <p>Your destination quality clothing and shoes</p>
 
       
-          {" "}
           <Link href={"/"}   className="footer-tag"> Home</Link>
         
-          {" "}
           <Link href={"/about"} className="footer-tag"  > About</Link>
         
-        
-          {" "}
           <Link href={"/shop"} className="footer-tag" > shop</Link>
         
-        
-          {" "}
           <Link href={"/contact"} className="footer-tag"> Contact</Link>
-        
-        
+      
           <Link href={"/privacy"}  className="footer-tag"> Privacy Policy</Link>
       
-        
-          {" "}
           <Link href={"/terms"} className="footer-tag"> T & C&apos;s</Link>
         
-      
-      
-
+  
       <div className="footer-flex-contact">
         <Image src={"/call.svg"} width={15} height={20} alt="call svg" />
         <Link href={"tel: +233 240 000000"}> +233 240 000000</Link>
+      </div>
       </div>
     
 
