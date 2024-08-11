67969cd6f02a5b86056e583c5b807ab8d2a2ceb3
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-11 09:43:08 +0000

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -38,7 +38,7 @@ const ShopPage = ({ products }) => {
         />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="author" content="Jeffery Agyepong" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,maximum-scale=1" />
         <link rel="icon" href="" />
       </Head>
 
