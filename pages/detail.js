45461880381e229b664567258860d56eaa33ab46
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-11 16:37:33 +0000

    Update detail.js

--- pages/detail.js
+++ pages/detail.js
@@ -1,74 +1,67 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import Header from "../components/Head/Header";
-import Footer from "../components/Footer/Footer";
-import CategoryCard from "../components/Products/ CategoryCard";
+import ProductCard from "../components/Products/ProductCard";
 import styles from "../sass/components/ShopPage.module.scss";
 import { getProducts } from "./api/products/index";
+import FootBottom from "@/components/Footer/FootBottom";
 import Nav from "@/components/Head/Nav";
 
-import Toast from "../components/Head/Toast";
-
-
-
-const Home = ({ products }) => {
+const DetailPage = ({ products }) => {
   return (
     <>
       <Head>
-        <title>Coast Republic | T-shirts and more</title>
+        <title>Shop | Coast Republic</title>
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@coastrepublicgh" />
         <meta name="twitter:creator" content="@coastrepublicgh" />
         <meta name="twitter:title" content="Coast Republic inc" />
-        <meta
-          name="twitter:description"
-          content="T-shirts, Sneakers & more.... "
-        />
+        <meta name="twitter:description" content="T-shirt, shoes and more..." />
         <meta
           name="twitter:image"
-          content="https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          content="https://i.postimg.cc/hjRpfKgJ/crlogo-1.png"
         />
         <meta property="og:title" content="Coast Republic inc" />
+        <meta property="og:description" content="T-shirt, shoes and more..." />
         <meta
-          property="og:description"
-          content="T-shirts, Sneakers & more.... "
+          property="og:url"
+          content="https://coast-republic.vercel.app/shop"
         />
-        <meta property="og:url" content="https://coast-republic.vercel.app/" />
         <meta
           property="og:image"
-          content="https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          content="https://i.postimg.cc/hjRpfKgJ/crlogo-1.png"
         />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
-
-        <meta name="description" content="Coast Republic  Store" />
         <meta
           name="keywords"
           content=" e-commerce, T-sirts , Ghana, Quality T-shirts, Clothing, Affordable clothing, crew neck, T-shirt print, store"
         />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <meta name="author" content="Coast Republic Inc" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/" />
-        <meta
-          name="google-site-verification"
-          content="HIhs3rvT7a6WD274_Txl6lfu3opycY_McRAFvT2-oBw"
-        />
+        <meta name="author" content="Jeffery Agyepong" />
+        <meta name="viewport" content="width=device-width,     initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+        <link rel="icon" href="" />
       </Head>
+
       <main>
-      
         <Header />
-        <Nav />
-        <div className={styles.cards}>
-          {products.map(product => (
-            <CategoryCard key={product.id} product={product} />
-          ))}
+        <Nav/>
+
+        <div className={styles.container}>
+        
+          <div className={styles.cards}>
+            {products.map(product => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         </div>
-        <Footer />
+
+ 
+        <FootBottom/>
       </main>
     </>
   );
 };
-export default Home;
+
+export default DetailPage;
 
 export async function getStaticProps() {
   const products = await getProducts();
