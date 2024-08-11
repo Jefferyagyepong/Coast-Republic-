c52011b2e666a28304822e985de99a18ac976943
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-11 09:49:00 +0000

    Update terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -36,7 +36,7 @@ export default function terms() {
         />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="author" content="Jeffery Agyepong" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, maximum-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
