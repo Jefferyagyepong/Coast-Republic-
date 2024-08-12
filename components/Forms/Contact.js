6a893b7010ee159bea8772ec19230f705caaca25
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-12 20:42:39 +0000

    Update Contact.js

--- components/Forms/Contact.js
+++ components/Forms/Contact.js
@@ -1,6 +1,6 @@
 export default function Contact() {
   return (
-    <div className="ibrid-box-align">
+    <div className="ibrid-box">
       <h3>Contact Us </h3>
       <p className="delivery-text">
         If you have any questions, please feel free to reach out via email. We
