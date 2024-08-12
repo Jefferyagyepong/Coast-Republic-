69187785613231db25b96197ae7a32a7a8901dad
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-12 22:26:02 +0000

    Update About.js

--- components/About.js
+++ components/About.js
@@ -1,6 +1,25 @@
 
 export default function About() {
   return (
-  
+    <div>
+      <h1>Coast Republic Inc.</h1>
+      <p>Established 2023.</p>
+      <p>
+        our physical store (inventory) is located in the Ashanti region of Ghana specifically Asokowa, kumasi. We
+        deliver the products we sell accross all 16 regions.
+      </p>
+      <h3>Discovery</h3>
+      <p>Online thrift store delivering  to your doorstep. 
+        Coast Republic offers our potential customers the opportunity to 
+          purchase thrift clothing and shoes, paying/checking out of system with 
+          payment gateway which include Visa card and Mobile money. We collect a few information 
+          when acustomer place an order. This information is very important and help us in successfully 
+          delivering items to customers. We encourage customers to provide us with accrate
+        information. Coast Republic is here to ease the stress our potential customers go through
+        assessing popular markets for thrifts items like Kantamanto in Accra and Adum in Kumasi.All your 
+        favorite brand products are available. 
+      
+      </p>
+    </div>
   );
 }
