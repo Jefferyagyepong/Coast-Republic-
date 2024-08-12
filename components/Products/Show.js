d63daa13ae8a3d453f9f657d71a9fa6e024b1586
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-12 15:37:36 +0000

    Update Show.js

--- components/Products/Show.js
+++ components/Products/Show.js
@@ -1,87 +1,20 @@
 import Image from "next/image";
-import Link from "next/link";
 export default function Show() {
    
   return (
-    <div>
-     
-  
-<section className="carousel" >
-  <ol className="carousel_viewport">
-    <li id="carousel_slide1"
-      
-        className="carousel_slide">
-      <div className="carousel_snapper">
-        <Link href={"#carousel_slide4"}
-           className="carousel_prev">Go to last slide</Link>
-        <Link href={"#carousel_slide2"}
-           className="carousel_next">Go to next slide</Link>
-      </div>
-    </li>
-    <li id="carousel_slide2"
-        
-        className="carousel_slide">
-      <div className="carousel_snapper"></div>
-      <Link href={"#carousel_slide1"}
-         className="carousel_prev">Go to previous slide</Link>
-      <Link href={"#carousel_slide3"}
-         className="carousel_next">Go to next slide</Link>
-    </li>
-    <li id="carousel_slide3"
-      
-        className="carousel_slide">
-      <div className="carousel_snapper"></div>
-      <Link href={"#carousel_slide2"}
-         className="carousel_prev">Go to previous slide</Link>
-      <Link href={"#carousel_slide4"}
-         className="carousel_next">Go to next slide</Link>
-    </li>
-    <li id={"carousel_slide4"}
-      
-        className="carousel_slide">
-      <div className="carousel_snapper"></div>
-      <Link href={"#carousel_slide3"}
-         className="carousel_prev">Go to previous slide</Link>
-      <Link href={"#carousel_slide1"}
-         className="carousel_next">Go to first slide</Link>
-    </li>
-  </ol>
-    <aside className="carousel_navigation">
-    <ol className="carousel_navigation-list">
-      <li className="carousel_navigation-item">
-        <Link href={"#carousel_slide1"}
-           className="carousel_navigation-button">Go to slide 1</Link>
-      </li>
-      <li className="carousel_navigation-item">
-        <Link href={"#carousel_slide2"}
-           className="carousel_navigation-button">Go to slide 2</Link>
-      </li>
-      <li className="carousel_navigation-item">
-        <Link href={"#carousel_slide3"}
-           className="carousel_navigation-button">Go to slide 3</Link>
-      </li>
-      <li className="carousel_navigation-item">
-        <Link href={"#carousel_slide4"}
-           className="carousel_navigation-button">Go to slide 4</Link>
-      </li>
-    </ol>
-  </aside>
-</section
 
-   </div>
-  
-  
-  
-  
-  
-  /*
-        <Image
+  <div class="carousel-container">
+  <div class="carousel-slide">
+       <Image
             src={"/products/vans1a.WEBP"}
             className="image-item"
             alt="img"
             width={70}
             height={60}
           />
+    </div>
+    <div class="carousel-slide">
+      
             <Image
             src={"/products/george1b.JPG"}
             className="image-item"
@@ -89,7 +22,9 @@ export default function Show() {
             width={70}
             height={60}
           />
-     
+      </div>
+      <div class="carousel-slide">
+        
                  <Image
             src={"/products/calvin1c.JPG"}
             className="image-item"
@@ -97,7 +32,9 @@ export default function Show() {
             width={70}
             height={60}
           />
-       
+        </div>
+        <div class="carousel-slide">
+          
                  <Image
             src={"/products/calvin1a.WEBP"}
             className="image-item"
@@ -105,7 +42,9 @@ export default function Show() {
             width={70}
             height={60}
           />
-      
+          </div>
+          <div class="carousel-slide">
+             
                  <Image
             src={"/products/true1a.JPG"}
             className="image-item"
@@ -114,6 +53,11 @@ export default function Show() {
             height={60}
           />
     
+            </div>
+          
+          <div class="carousel-slide">
+            
+     
                 <Image
             src={"/products/reebook1a.JPG"}
             className="image-item"
@@ -121,7 +65,20 @@ export default function Show() {
             width={70}
             height={60}
           />
-          */
+            
+            </div>
+  
+  
+</div>
+
+  
+  
+  
+  
+     
+     
+       
+        
     
   );
 }
