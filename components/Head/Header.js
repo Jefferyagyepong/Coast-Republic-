07f42b222fb57c1288e30cf915c537303478bee3
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-12 08:55:50 +0000

    Update Header.js

--- components/Head/Header.js
+++ components/Head/Header.js
@@ -1,1 +1,26 @@
+import React, { useRef, useLayoutEffect } from "react";
+import Image from "next/image";
+import CartBasket from "./CartBasket";
+import Link from "next/link.js";
 
+
+export default function Header() {
+
+  return (
+    <header>
+   
+        <Link href={"./"}>
+          <Image
+            src={"/crlogo.svg"}
+            width={90}
+            height={70}
+            alt="logo"
+          />
+        </Link>
+      
+        <CartBasket />
+ 
+  
+    </header>
+  );
+}
