ed6ffe7d4c98b5c4cb6aba330b0028dc0839b43d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-13 20:15:06 +0000

    Update About.js

--- components/About.js
+++ components/About.js
@@ -13,7 +13,7 @@ export default function About() {
         Coast Republic offers our potential customers the opportunity to 
           purchase thrift clothing and shoes, paying/checking out of system with 
           payment gateway which include Visa card and Mobile money. We collect a few information 
-          when acustomer place an order. This information is very important and help us in successfully 
+          when a customer places an order. This information is very important and help us in successfully 
           delivering items to customers. We encourage customers to provide us with accrate
         information. Coast Republic is here to ease the stress our potential customers go through
         assessing popular markets for thrifts items like Kantamanto in Accra and Adum in Kumasi.All your 
