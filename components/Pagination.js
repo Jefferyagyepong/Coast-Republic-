c12b15eac2c40fd1748d508a70143d800cf3af4a
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 20:14:27 +0000

    Update Pagination.js

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Pagination = ({ totalPages }) => {
 
