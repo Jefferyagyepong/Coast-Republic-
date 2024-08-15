51ea6785d26696012eb42e245436d0c0866e20c5
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 18:32:10 +0000

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -1,32 +1,33 @@
 
 import { getProducts } from "./api/products/index";
 export const getServerSideProps = async ({ query }) => {
-3  const page = query.page || 1;
-4  const res = await fetch(`https://coast-republic.vercel.app/api/products/data?page=${page}`);
-5  const data = await res.json();
-6
-7  return {
-8    props: {
-9      data,
-10      totalPages: data.totalPages,
-11      currentPage: page,
-12    },
-13  };
-14};
-15
-16const Page = ({ data, totalPages, currentPage }) => {
-17  return (
-18    <div>
-19      <Pagination totalPages={totalPages} currentPage={currentPage} />
-20      <ul>
-21        {data.items.map(item => (
-22          <li key={item.id}>{item.name}</li>
-23        ))}
-24      </ul>
-25    </div>
-26  );
-27};
-28
+  const page = query.page || 1;
+  const res = await fetch(`https://coast-republic.vercel.app/api/products/data?page=${page}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      products,
+      totalPages: products.totalPages,
+      currentPage: page,
+    },
+  };
+};
+
+const Page = ({ products, totalPages, currentPage }) => {
+  return (
+    <div>
+      <Pagination totalPages={totalPages} currentPage={currentPage} />
+       <div className={styles.cards}>
+            {products.map(product => (
+              <CategoryCard key={product.id} product={product} />
+            ))}
+          </div>
+
+  </div>
+  );
+};
+
 export default Page;
 ​
 
