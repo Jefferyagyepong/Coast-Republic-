aea6c589c00d3024ea358b2ba9ea65afd8d5850c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 03:09:25 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -21,8 +21,8 @@ function Sale() {
         <Image className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
          <Image  className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
         <Image  className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1620794341491-76be6eeb6946?q=80&w=788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-      <button className="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-<button className="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+      <button className="w3-button w3-display-left" onclick="plusDivs(-1)"></button>
+<button className="w3-button w3-display-right" onclick="plusDivs(+1)"></button>
 </div>
  </div>
  
