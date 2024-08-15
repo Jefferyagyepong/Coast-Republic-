caa6f1521fa2da1af900461083d784c0c7126b99
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 18:11:05 +0000

    Update Pagenation.js

--- components/Pagenation.js
+++ components/Pagenation.js
@@ -1,42 +1,38 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
 
-const Pagination = ({ totalPages }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const router = useRouter();
-
-  const handlePageChange = page => {
-    setCurrentPage(page);
-    router.push(`?page=${page}`, undefined, { shallow: true });
-  };
-
-  return (
-    <div className="pagination">
-      <button
-        onClick={() => handlePageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-       
-      >
-        Previous
-      </button>
-      {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
-        <button
-          key={page}
-          onClick={() => handlePageChange(page)}
-          className={currentPage === page ? "active" : ""}
-        >
-          {page}
-        </button>
-      ))}
-      <button
-        onClick={() => handlePageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-       
-      >
-        Next
-      </button>
-    </div>
-  );
-};
-
-export default Pagination;
+import { useRouter } from 'next/router';
+2
+3const Pagination = ({ totalPages, currentPage }) => {
+4  const router = useRouter();
+5
+6  const handlePageChange = (page) => {
+7    router.push(`?page=${page}`, undefined, { shallow: true });
+8  };
+9
+10  return (
+11    <div className="pagination">
+12      <button
+13        onClick={() => handlePageChange(currentPage - 1)}
+14        disabled={currentPage === 1}
+15      >
+16        Previous
+17      </button>
+18      {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+19        <button
+20          key={page}
+21          onClick={() => handlePageChange(page)}
+22          className={currentPage === page ? 'active' : ''}
+23        >
+24          {page}
+25        </button>
+26      ))}
+27      <button
+28        onClick={() => handlePageChange(currentPage + 1)}
+29        disabled={currentPage === totalPages}
+30      >
+31        Next
+32      </button>
+33    </div>
+34  );
+35};
+36
+37export default Pagination;
