20efd6a71f3da225e9e17f4859ce906981db5188
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 02:16:16 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -24,7 +24,7 @@ function Sale() {
         </div>
       </div>
       <div class="image">
-        <Image  alt="product"  width={370} height={350} src={"https://unsplash.com/photos/black-and-green-cruiser-board-near-black-nike-shoes-eyhC3UQHQgc"} />
+        <Image  alt="product"  width={370} height={350} src={"https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
 <Image alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
 <Image   alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
 <Image   alt="product" width={370} height={350} src={"https://unsplash.com/photos/unpaired-red-adidas-sneaker-pO2bglTMJpo"} />
