299f5d2df5aa6aa43f1b5a5476f34679744b6548
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 01:20:37 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -1,9 +1,29 @@
  import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from 'react';
 
-export default function Sale() {
+function Sale() {
+  useEffect(() => {
+ var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+  }, []);
+
   return (
-    <div className="shop-now">
-      <h3 className="next-color-option">NEW SEASON</h3>
+   <div className="shop-now">
+
+   <h3 className="next-color-option">NEW SEASON</h3>
 
       <h2 className="next-color">
         Discount automatically applied at check out
@@ -12,6 +32,18 @@ export default function Sale() {
       <Link href={"/shop"} className="add-to-cart">
         Shop Now
       </Link>
+ <Image class="mySlides" src={"https://unsplash.com/photos/black-and-green-cruiser-board-near-black-nike-shoes-eyhC3UQHQgc"}>
+<Image class="mySlides" src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}>
+<Image class="mySlides" src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}>
+<Image class="mySlides" src={"https://unsplash.com/photos/unpaired-red-adidas-sneaker-pO2bglTMJpo"}>
+    
+    
+
+    
+   
     </div>
+ 
   );
 }
+
+export default Sale;
