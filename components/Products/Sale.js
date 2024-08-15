8d7bddd5d829cfe796432648d8fd77bb72963696
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 01:55:21 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -1,30 +1,18 @@
  import Link from "next/link";
 import Image from "next/image";
-import { useEffect } from "react";
 
-export default function Sale() {
-  useEffect(() => {
- var slideIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); 
-}
-  }, []);
-
+function Sale() {
   return (
   
-   <div className="shop-now">
-
-   <h3 className="next-color-option">NEW SEASON</h3>
+<div id="slider">
+  <div class="slides">
+	
+    <div class="slider">
+      <div class="legend"></div>
+      <div class="content">
+        <div class="content-txt">
+             <h3 className="next-color-option">NEW SEASON</h3>
 
       <h2 className="next-color">
         Discount automatically applied at check out
@@ -33,16 +21,32 @@ function carousel() {
       <Link href={"/shop"} className="add-to-cart">
         Shop Now
       </Link>
- <Image className="mySlides" src={"https://unsplash.com/photos/black-and-green-cruiser-board-near-black-nike-shoes-eyhC3UQHQgc"}>
+        </div>
+      </div>
+      <div class="image">
+        <Image className="mySlides" src={"https://unsplash.com/photos/black-and-green-cruiser-board-near-black-nike-shoes-eyhC3UQHQgc"}>
 <Image className="mySlides" src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}>
 <Image className="mySlides" src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}>
 <Image className="mySlides" src={"https://unsplash.com/photos/unpaired-red-adidas-sneaker-pO2bglTMJpo"}>
-        <button className="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-<button className="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
-
-       
+      </div>
     </div>
+ 
+  </div>
+  <div class="switch">
+    <ul>
+      <li>
+        <div class="on"></div>
+      </li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</div>
+
+
  
   );
 }
+export default Sale;
  
