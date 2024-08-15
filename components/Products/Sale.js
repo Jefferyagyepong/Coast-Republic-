72fb8d836be70f93eddcd231c23e4b6506172983
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 01:37:34 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -16,7 +16,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 2000); 
 }
   }, []);
 
