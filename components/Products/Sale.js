16ca4ce9ae247019323471ee455e266c098be7b2
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 01:35:45 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -12,7 +12,7 @@ function carousel() {
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-  }
+
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
