4c75d3e64a8d00b367281a95d99768660fa12d32
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 03:25:52 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -5,23 +5,36 @@ function Sale() {
     <div className="container-slider">
 
  <div className="wrapper">
-             <h3>NEW SEASON</h3>
-      <h2>
-        Discount automatically applied at check out
-      </h2>
+    
+      
+      <div>
+         <Image className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+    
+      <Link href={"/shop"} className="add-to-cart">
+        Shop Now
+      </Link>
+        </div>
+       <div>
+        <Image  className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1620794341491-76be6eeb6946?q=80&w=788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
 
       <Link href={"/shop"} className="add-to-cart">
         Shop Now
       </Link>
-      
-      
-         <Image className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-    
+        </div>
+       <div> 
+        <Image  className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+      <Link href={"/shop"} className="add-to-cart">
+        Shop Now
+      </Link>
+        </div>
+       <div>
+         
         <Image className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-         <Image  className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-        <Image  className="mySlides" alt="product" width={370} height={350} src={"https://images.unsplash.com/photo-1620794341491-76be6eeb6946?q=80&w=788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-
-      
+        
+      <Link href={"/shop"} className="add-to-cart">
+        Shop Now
+      </Link>
+        </div>      
 </div>
  </div>
  
