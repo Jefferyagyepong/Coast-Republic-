4a25fcefcca6a2d2ca859658e21d1ce3aa34d5f4
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 18:43:31 +0000

    Update Pagination.js

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,1 +1,37 @@
-h
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Pagination = ({ totalPages, currentPage }) => {
+  const router = useRouter();
+
+  const handlePageChange = (page) => {
+    router.push(`?page=${page}`, undefined, { shallow: true });
+  };
+ return (
+   <div className="pagination">
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+       disabled={currentPage === 1}
+      >
+       Previous
+      </button>
+      {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+        <button
+          key={page}
+          onClick={() => handlePageChange(page)}
+          className={currentPage === page ? 'active' : ''}
+       >
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination
