bcfc776213cc100cd4a4e0ce21d5e8577705ba56
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 15:42:56 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -6,7 +6,9 @@ function Sale() {
  
 <div className="slides slowFade">
         <div className="slide">
-             <Image  alt="product" src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+          <h2>New Season</h2>
+          <Link href={"/shop"} className="add-to-cart">Shop</Link>
+          <Image  alt="product" src={"https://images.unsplash.com/photo-1554350747-ec45fd24f51b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
         </div>
         <div className="slide">
             
@@ -17,16 +19,12 @@ function Sale() {
         </div>
         <div className="slide">
              <Image   alt="product" src={"https://images.unsplash.com/photo-1620794341491-76be6eeb6946?q=80&w=788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-
         </div>
         <div className="slide">
              <Image  alt="product" src={"https://images.unsplash.com/photo-1531123414780-f74242c2b052?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
-  
-       
+      
           </div>
     </div>
-
-
 
 
   );
