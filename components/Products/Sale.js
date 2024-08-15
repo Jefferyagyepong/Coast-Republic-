33aa1af9451ce095de1548cfca4813ca77b5cc86
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-15 18:47:59 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -24,7 +24,7 @@ function Sale() {
       
           </div>
           <div className="slide">
-            <Image  src={"https://images.unsplash.com/photo-1704949841973-9db544ac35ec?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+            <Image  alt=""src={"https://images.unsplash.com/photo-1704949841973-9db544ac35ec?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
             </div>
               <div className="slide">
              <h2>New Season</h2>
