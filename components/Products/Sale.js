83abc269fabbf2e6f946bed9a21bb57d35024a54
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-25 20:36:20 +0000

    Update Sale.js

--- components/Products/Sale.js
+++ components/Products/Sale.js
@@ -5,8 +5,8 @@ function Sale() {
     <div className="slides slowFade">
       <div className="slide">
         <Image
-          width={140}
-          height={90}
+          width={500}
+          height={600}
           alt="product"
           src={
             "https://images.unsplash.com/photo-1577983127195-95ada1fa7387?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -16,8 +16,8 @@ function Sale() {
 
       <div className="slide">
         <Image
-          width={140}
-          height={90}
+          width={500}
+          height={600}
           alt="product"
           src={
             "https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -27,8 +27,8 @@ function Sale() {
 
       <div className="slide">
         <Image
-          width={140}
-          height={90}
+        width={500}
+          height={600}
           alt="product"
           src={
             "https://images.unsplash.com/photo-1508507031248-e96f9cd82522?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -38,8 +38,8 @@ function Sale() {
 
       <div className="slide">
         <Image
-          width={140}
-          height={90}
+         width={500}
+          height={600}
           alt="product"
           src={
             "https://images.unsplash.com/photo-1598457577935-662aa6d06382?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -49,8 +49,8 @@ function Sale() {
 
       <div className="slide">
         <Image
-          width={140}
-          height={90}
+     width={500}
+          height={600}
           alt="product"
           src={
             "https://images.unsplash.com/photo-1704949841973-9db544ac35ec?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
