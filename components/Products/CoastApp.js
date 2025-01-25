fe74e09b14f814a33e1343707fb3b5c72716373f
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-25 13:44:58 +0000

    Update CoastApp.js

--- components/Products/CoastApp.js
+++ components/Products/CoastApp.js
@@ -1,12 +1,12 @@
 import Image from 'next/image';
  function IbridApp() {
     return (
-      <div className="ibrid-box bgg222"> 
-        <h3>Download Our App</h3><br/>
+      <div className="ibrid-box bgg222"> <br/>
+        <h3>Download Our App</h3><br/><br/>
         <p>
           Unlock a world of limetless possiblities. Download the Coast Republic
           app and embark on a journey of discovery and connection.
-        </p>
+        </p><br/>
 
         <div className="ibrid-flex">
           <Image src={"/apple.svg"} alt="apple" width={70} height={100} />
