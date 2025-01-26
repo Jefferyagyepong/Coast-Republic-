e5cf3bdff4e985c51a160cb42ca99199bbc238ff
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-26 14:50:24 +0000

    Update Terms.js

--- components/Footer/Terms.js
+++ components/Footer/Terms.js
@@ -488,10 +488,7 @@ function Terms() {
       </p>
                 </section>
                </article>
-                <h6 className="footer-tag">
-        &copy; {new Date().getFullYear()} Coast Republic All rights Reserved
-      </h6> <br/>
-              
+     
 
     </div>
   );
