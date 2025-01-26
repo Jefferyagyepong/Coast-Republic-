89c7fb9975a2bb1d07c05b0eb5e7cfb0ab6282bd
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-26 20:58:45 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useState, useEffect } from 'react';
 import Thrifts from "../components/Products/Thrifts";
 import New from "../components/Products/New";
 import Header from "../components/Head/Header";
