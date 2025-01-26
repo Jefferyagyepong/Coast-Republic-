86425d275ed988338fc30d1679e2affca959a3d7
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-26 15:34:34 +0000

    Update Privacy.js

--- components/Footer/Privacy.js
+++ components/Footer/Privacy.js
@@ -325,6 +325,9 @@ function Privacy() {
       </p>
                 </section>
                </article>
+               <h6 className="footer-tag">
+        &copy; {new Date().getFullYear()} Coast Republic All rights Reserved
+      </h6> <br/>
  
     </div>
   );
