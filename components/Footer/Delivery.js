f1a30ec846493ce0bcd9013b869e43640f402ce6
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-26 13:16:52 +0000

    Update Delivery.js

--- components/Footer/Delivery.js
+++ components/Footer/Delivery.js
@@ -21,6 +21,9 @@ function Delivery() {
       </b>
            </section>
         </article>
+          <h6 className="footer-tag">
+        &copy; {new Date().getFullYear()} Coast Republic All rights Reserved
+      </h6> <br/>
 
     </div>
   );
