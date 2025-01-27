ff9420f568e61b5fb3d60f33665280e191b6e0c9
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-27 17:55:04 +0000

    Update index.js

--- pages/products/index.js
+++ pages/products/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import fs from 'fs';
 import path from 'path';
+import Head from 'next/head';
 
 export async function getStaticProps() {
   // Read the products JSON file from the public directory
@@ -19,6 +20,30 @@ export async function getStaticProps() {
 
 const ProductList = ({ products }) => {
   return (
+    <>
+          <Head>
+        <title>Coast Republic | T-shirts and more</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@coastrepublicgh" />
+        <meta name="twitter:creator" content="@coastrepublicgh" />
+        <meta name="twitter:title" content="Coast Republic inc" />
+        <meta name="twitter:description" content="T-shirts, Sneakers & more.... "/>
+        <meta name="twitter:image" content="https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+        <meta property="og:title" content="Coast Republic inc" />
+        <meta property="og:description" content="T-shirts, Sneakers & more.... " />
+        <meta property="og:url" content="https://coast-republic.vercel.app/" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+       <link rel="apple-touch-icon" href="/favicon.ico" />
+
+        <meta name="description" content="Coast Republic  Store" />
+        <meta name="keywords" content=" e-commerce, T-sirts , Ghana, Quality T-shirts, Clothing, Affordable clothing, crew neck, T-shirt print, store"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="author" content="Coast Republic Inc" />
+        <meta  name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+        <link rel="icon" href="/" />
+        <meta name="google-site-verification" content="HIhs3rvT7a6WD274_Txl6lfu3opycY_McRAFvT2-oBw"/>
+      </Head>
+      <main>
     <div>
       <h1>Product List</h1>
       <ul>
@@ -39,6 +64,8 @@ const ProductList = ({ products }) => {
         ))}
       </ul>
     </div>
+    <main>
+    </>
   );
 };
 
