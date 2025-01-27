7a1fc99874fb28fd4d0eb6b0287c5b490c596451
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-27 23:34:25 +0000

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -70,7 +70,7 @@ const CartPage = () => {
           {cart.length === 0 ? (
             <div>
               <h3>Your Cart is Empty!</h3>
-              <Link href={"/shop"}>click here to sho now</Link>
+              <Link href={"/products"}>click here to sho now</Link>
             </div>
           ) : (
             <>
