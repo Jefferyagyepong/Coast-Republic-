711611d6ea13d17629c2f58b42ef9faf9d778339
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-27 12:39:37 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Home = ({ products }) => {
         <meta name="twitter:site" content="@coastrepublicgh" />
         <meta name="twitter:creator" content="@coastrepublicgh" />
         <meta name="twitter:title" content="Coast Republic inc" />
-        <meta name="twitter:description" content="T-shirts, Sneakers & more.... ">
+        <meta name="twitter:description" content="T-shirts, Sneakers & more.... "/>
         <meta name="twitter:image" content="https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
         <meta property="og:title" content="Coast Republic inc" />
         <meta property="og:description" content="T-shirts, Sneakers & more.... " />
