e19d112a90c1085f54ffd394b937a1aadb7722e9
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-28 09:56:22 +0000

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -56,7 +56,7 @@ function Footer() {
       <div className="payment-container">
             
                <Image
-          src={"/telecel.jpg"}
+          src={"/telecel.JPG"}
           height={30}
           width={35}
           alt="momo logo"
