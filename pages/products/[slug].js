309cd469d5240c98a8f35eaf797d60a7161571d9
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-28 18:13:11 +0000

    Update [slug].js

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,3 +1,4 @@
+import Header from "../components/Head/Header";
 import Toast from "../components/Head/Toast";
 import Newsletter from "@/components/Footer/Newsletter";
 import Head from "next/head";
