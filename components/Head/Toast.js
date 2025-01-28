1960853c43dde7469036deabb5278c68abf4ea9d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-28 00:10:36 +0000

    Update Toast.js

--- components/Head/Toast.js
+++ components/Head/Toast.js
@@ -1,13 +1,32 @@
- function Toast() {
+ 
+
+
+import { useState, useEffect } from 'react';
+
+const FadeText = () => {
+  const [visible, setVisible] = useState(false);
+
+  // Toggle the text visibility every 2 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVisible((prev) => !prev);
+    }, 2000); // Toggle every 2 seconds (2000ms)
+
+    // Clear interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="example1">
-   
-      <strong>
-        {" "}
-        <h6> We deliver to all 16 regions 🇬🇭</h6>
-    
-      </strong>
+    <div className=example1"">
+      <strong><p className={`fade-text ${visible ? 'visible' : 'hidden'}`}></strong>
+        Akwaaba!! We deliver to all 16 regions 
+      </p>
     </div>
   );
- }
-export default Toast;
+};
+
+
+};
+
+export default FadeText;
+
