ea42de2145dd4efe64877b0d5ca0fe22f16c3199
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-28 09:31:07 +0000

    Update Toast.js

--- components/Head/Toast.js
+++ components/Head/Toast.js
@@ -1,2 +1,14 @@
-
+ function Toast() {
+  return (
+    <div className="example1">
+   
+      <strong>
+        {" "}
+        <h6> We deliver to all 16 regions 🇬🇭</h6>
+    
+      </strong>
+    </div>
+  );
+ }
+export default Toast;
 
