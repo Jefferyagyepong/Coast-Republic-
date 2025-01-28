28e16b39633d9d20b857979e7af11d864648e4d1
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-28 15:54:31 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-             <script
-            src="https://js.paystack.co/v1/inline.js"
-            type="text/javascript"
-          />
-        </head>
+      <Head />
+       
+        
       <body>
         <Main />
         <NextScript />
