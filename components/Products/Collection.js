125ecc1b0471ed67ea360015ac18c6b64ead6403
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:44:56 +0000

    Update Collection.js

--- components/Products/Collection.js
+++ components/Products/Collection.js
@@ -1,7 +1,14 @@
 export default function Collection() {
   return (
-    <div className="ibrid-box"><br/><br/>
- <h3> </h3>
-    </div>
+ <div className="ibrid-box">
+    <div className="jean-bg">
+        
+     </div>
+    
+<h2>Jeans: Designer pairs in our collection </h2>
+<Link href={"/products"}>Shop now</Link>
+
+
+     </div>
   );
 }
