4ca740e9b3b319236551220e531a72154b11276c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 19:10:58 +0000

    Update About.js

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
-function About() {
+function Company() {
   return (
-    <>
+   <div>
         <h1 >About Us</h1>
         
         <section>
@@ -26,7 +26,7 @@ function About() {
             Have questions? We would love to hear from you! Contact us at <a href="mailto:support@ecommerce.com" className="text-blue-600">support@ecommerce.com</a>.
           </p>
         </section>
-        </>
+        </div>
   );
 }
-export default About;
+export default Company;
