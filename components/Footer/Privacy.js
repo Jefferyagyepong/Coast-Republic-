11cc57d9e74cd0bcfe80d69dc53711757653d98c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 18:02:26 +0000

    Update Privacy.js

--- components/Footer/Privacy.js
+++ components/Footer/Privacy.js
@@ -4,18 +4,27 @@ function Privacy() {
       <article>
   <h3>Effective Date: 20-01-2025</h3>
 
-<p>COAST REPUBLIC INC. ("we," "our," or "us") respects your privacy and is committed to protecting the personal information you provide when using our website and services. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website www.coast-republic.com, use our mobile application, or engage with our services.
-
-By using our platform, you agree to the terms of this Privacy Policy. If you do not agree, please do not use our services.</p>
-
+<p>COAST REPUBLIC INC. ("we," "our," or "us") respects
+   your privacy and is committed to protecting the personal
+   information you provide when using our website and services. 
+  This Privacy Policy explains how we collect, use, disclose, 
+  and safeguard your information when you visit our website
+   www.coast-republic.com, use our mobile application, 
+  or engage with our services. By using our platform, 
+  you agree to the terms of this Privacy Policy. 
+  If you do not agree, please do not use our services.</p>
 </article>
+
 <article>
 <h4>1.Information We Collect</h4>
 
-<p>We collect different types of information when you interact with our e-commerce website:</p>
-
+<p>We collect different types of information
+ when you interact with our e-commerce website:</p>
+</article>
+<article>
 <h5>a. Personal Information</h5>
-<p>When you create an account, place an order, or contact us, we may collect:</p>
+<p>When you create an account, place 
+an order, or contact us, we may collect:</p>
 <ul>
   <li>
     Name
@@ -32,13 +41,17 @@ By using our platform, you agree to the terms of this Privacy Policy. If you do 
   
 </ul>
 
-<p>Payment details (processed securely by third-party payment processors)</p>
+<p>Payment details (processed securely by 
+  third-party payment processors)</p>
+  
 </article>
+
 
 <article>
  
 <h5>b. Non-Personal Information</h5>
-<p>We automatically collect data when you visit our site, including:</p>
+<p>We automatically collect data when you 
+  visit our site, including:</p>
 
 <ul>
   <li>
@@ -59,24 +72,22 @@ By using our platform, you agree to the terms of this Privacy Policy. If you do 
 </ul>
 </article>
 
+
+
 <article>
   <h5>c. Cookies and Tracking Technologies</h5>
-<p>We use cookies and similar technologies to improve user experience, analyze site traffic, and personalize content. You can manage cookie preferences through your browser settings.</p>
-
+<p>We use cookies and similar technologies to 
+  improve user experience, analyze site traffic, 
+  and personalize content. You can manage cookie 
+  preferences through your browser settings.</p>
 
 </article>
 
 
-
-
-
-
-
 <article>
-
 <h5>2. How We Use Your Information</h5>
-
 <p>We use your data for the following purposes:</p>
+
 <ul>
   <li>
     To process and fulfill your orders
@@ -91,38 +102,35 @@ By using our platform, you agree to the terms of this Privacy Policy. If you do 
     To prevent fraud and secure transactions
   </li>
    <li>    
-To comply with legal obligations
+  To comply with legal obligations
   </li>
 </ul>
-
-
-
+</article>
 
 <article>
-  <h5>3. How We Share Your Information</h5>
-
-<p>We do not sell your personal data. However, we may share it with:</p>
-
+ <h5>3. How We Share Your Information</h5>
+<p>We do not sell your personal data.
+ However, we may share it with:</p>
 <p>Third-party service providers (e.g., payment processors, shipping partners)
 Legal authorities if required by law or for fraud prevention
 Marketing partners (only with your consent)</p>
-
 </article>
 
-<article>
-  
-  
+<article>    
 <h5>4. Data Security</h5>
-
-<p>We take appropriate measures to protect your personal information from unauthorized access, alteration, or disclosure. However, no method of transmission over the internet is 100% secure, so we cannot guarantee absolute security.</p>
+<p>We take appropriate measures to protect
+   your personal information from unauthorized 
+  access, alteration, or disclosure. 
+  However, no method of transmission 
+  over the internet is 100% secure, 
+  so we cannot guarantee absolute security.</p>
+  
 </article>
 
-
-
 <article>
-  <h5>5. Your Rights and Choices</h5>
-
-<p>Depending on your location, you may have the following rights:</p>
+<h5>5. Your Rights and Choices</h5>
+<p>Depending on your location, you may have 
+  the following rights:</p>
 <ul>
 <li>
   Access & Update: Request access to or update your data.
@@ -135,60 +143,50 @@ Marketing partners (only with your consent)</p>
 </li>
 <li>
   Cookies: Manage cookie preferences via your browser.
-</li>
-
-  
+</li>  
 </ul>
 
-
-<p> To exercise your rights, contact us at info@coastrepublic.com
-
-</p>
-
-
-
+<p> To exercise your rights, contact us at info@coastrepublic.com</p>
 </article> 
 
 <article> 
-  <h5>6. Third-Party Links</h5>
-
-<p>Our website may contain links to third-party websites. We are not responsible for their privacy policies or practices. Please review their policies before providing any personal data.</p>
-
-
+<h5>6. Third-Party Links</h5>
+<p>Our website may contain links to 
+  third-party websites. We are not responsible
+  for their privacy policies or practices. 
+  Please review their policies before providing
+  any personal data.</p>
 </article> 
 
-<article>
-  
-  <h5>7. Children's Privacy</h5>
 
-<p>Our services are not intended for children under 13 (or the applicable age in your country). We do not knowingly collect data from children. If you believe a child has provided us with personal data, please contact us to remove it.</p>
+<article>  
+<h5>7. Children's Privacy</h5>
+<p>Our services are not intended for
+   children under 13 (or the applicable
+   age in your country). We do not knowingly 
+  collect data from children. If you believe
+   a child has provided us with personal data, 
+  please contact us to remove it.</p>
 </article>
 
 
 <article>
-  <h5>8. Changes to This Privacy Policy</h5>
-
-<p>We may update this Privacy Policy from time to time. Any changes will be posted on this page with the updated Effective Date.</p>
-
-
+<h5>8. Changes to This Privacy Policy</h5>
+<p>We may update this Privacy Policy from
+   time to time. Any changes will be posted 
+  on this page with the updated Effective Date.</p>
 </article>
-
 
 <article>
-  <h5>9. Contact Us</h5>
+<h5>9. Contact Us</h5>
+<p>For any questions or concerns about this
+ Privacy Policy, please contact us at:</p>
 
-<p>For any questions or concerns about this Privacy Policy, please contact us at:</p>
-
-COAST REPUBLIC
-Email: info@coastrepublicgh.com
-Address: 7 Minneti ST, Asokwa
-
-
+<b>COAST REPUBLIC</b>
+<b>Email: info@coastrepublicgh.com</b>
+<b>Address: 7 Minneti ST, Asokwa</b>
 </article>
-<ul>
-  <p>
-  </p>
-</ul>
+
 
 
 
