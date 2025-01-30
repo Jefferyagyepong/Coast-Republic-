4412ecde950fc97c2713646139dad19bc8b5d468
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:49:17 +0000

    Update Title.js

--- components/Products/Title.js
+++ components/Products/Title.js
@@ -2,12 +2,16 @@
 
 function Title() {
   return (
-    <div className="ibrid-box">
-      <em>
-        {" "}
-        <h2>Stories</h2>
-      </em>
-    </div>
+ <div  className="footer-align-left">
+    <div className="jean-bg">
+        
+     </div>
+    
+<h2>Sneakers: Designer pairs in our collection </h2>
+<Link href={"/products"}>Shop now</Link>
+
+
+     </div>
   );
 }
 export default Title;
