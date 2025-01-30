feed99a50d9fb7a99c1ce67de1c86afccbe95cde
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 02:53:46 +0000

    Update Jeans.js

--- components/Products/Jeans.js
+++ components/Products/Jeans.js
@@ -6,7 +6,12 @@ const images = [
 function Show (){
   return(
  <div  className="footer-align-left">
- 
+  
+      <div>
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`Slide ${index}`} className="slide" />
+        ))}
+      </div>
     
 <h5>Jeans: Designer pairs in our collection </h5>
 <Link href={"/products"}>Shop now</Link>
