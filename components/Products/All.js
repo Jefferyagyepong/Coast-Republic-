8f21fbd89cb59a936a903b53b2ee0ab64440e919
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:54:48 +0000

    Update All.js

--- components/Products/All.js
+++ components/Products/All.js
@@ -3,10 +3,7 @@
  function All() {
   return (
  <div  className="footer-align-left">
-    <div className="jean-bg">
-        
-     </div>
-    
+ 
 <h2>Journal: Designer pairs in our collection </h2>
 <Link href={"/products"}>Shop now</Link>
 
