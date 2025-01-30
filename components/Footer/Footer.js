8c1bc82e636a61456288219f8a76b14551aa9e1f
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:17:25 +0000

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,22 +6,7 @@ function Footer() {
     <footer>
       <br/>  <br/>
       <div className="footer-align-left">
-        <em>
-          <Link href={"./"}>
-            <h2 className="footer-tag">Coast Republic Inc.</h2>
-          </Link>
-          <address className="footer-tag">
-            <h6> AK-717-5846 Minetti ST, Asokwa 🇬🇭</h6><br/>
-            <h6>info@coastrepublic.com</h6><br/>
-            <div className="footer-flex-contact">
-              <Image src={"/call.svg"} width={15} height={20} alt="call svg" />
-              <Link href={"tel: +233 240 000000"} className="footer-tag">
-                {" "}
-                +233 240 000000
-              </Link>
-            </div>
-          </address>
-        </em><br/>  <br/>
+ 
         <b>
           <Link href={"/"} className="footer-tag">
             {" "}
