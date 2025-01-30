2676b47481676159729f315ff6e1f401a33ce19a
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 12:06:17 +0000

    Update Carousel.js

--- components/Products/Carousel.js
+++ components/Products/Carousel.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { useRef, useState } from "react";
 
 const images = [
-  "/thugga.jpg",
+  " /images/image2.jpg",
   "/images/image2.jpg",
   "/images/image3.jpg",
   "/images/image4.jpg",
