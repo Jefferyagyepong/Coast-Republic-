49ddc98467c282d3780ae9a7f2e8c46d6950e681
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:53:47 +0000

    Update Jeans.js

--- components/Products/Jeans.js
+++ components/Products/Jeans.js
@@ -3,9 +3,7 @@ import Image from 'next/image';
 function Show (){
   return(
  <div  className="footer-align-left">
-    <div className="jean-bg">
-        
-     </div>
+ 
     
 <h2>Jeans: Designer pairs in our collection </h2>
 <Link href={"/products"}>Shop now</Link>
