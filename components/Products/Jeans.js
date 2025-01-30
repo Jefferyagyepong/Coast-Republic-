80a7355546812fb729816615cb70384092f294bc
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 08:17:44 +0000

    Update Jeans.js

--- components/Products/Jeans.js
+++ components/Products/Jeans.js
@@ -1,21 +1,14 @@
 import Link from 'next/link';
-import Image from 'next/image';
-  
-function Show (){
-  return(
- <div className="pdt-box">
-  <Image src="/thugga.jpg" width={480} height={600}  alt="pic"/>
-  
-      
-    
-<h5>Jeans: Designer pairs in our collection </h5>
+
+ function Show() {
+  return (
+ <div  className="footer-align-left">
+ 
+<h5>Journal: Designer pairs in our collection </h5>
 <Link href={"/products"}>Shop now</Link>
 
- 
+
      </div>
-    
-    
-  
   );
-}
+ }
 export default Show;
