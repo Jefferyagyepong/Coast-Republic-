36ca8b40c0ca4a031e0ae4ef587dc87c9f83f502
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 12:37:56 +0000

    Update Newsletter.js

--- components/Footer/Newsletter.js
+++ components/Footer/Newsletter.js
@@ -39,6 +39,12 @@ const SubscribeForm = () => {
   return (
     <div className="footer-align-left">
       <h4>SUBSCRIBE TO OUR NEWSLETTER </h4>
+     <p>
+    Stay in the loop with the latest style news and get an exclusive 10% off when you subscribe to our emails.
+Exclusions apply. Learn more about our Privacy Policy 
+<Link href={"/privacy"}> here.</Link><Link href={"/terms"}>Terms and conditions </Link>
+  apply.
+      </p>
     
         
       {success && <p>Thanks for subscribing!</p>}
