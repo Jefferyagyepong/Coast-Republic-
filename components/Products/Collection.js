e1aae46e7a927e474a98b17011b8fc92f7869f0c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:53:06 +0000

    Update Collection.js

--- components/Products/Collection.js
+++ components/Products/Collection.js
@@ -1,9 +1,7 @@
 export default function Collection() {
   return (
  <div  className="footer-align-left">
-    <div className="jean-bg">
-        
-     </div>
+ 
     
 <h2>Tees: Designer pairs in our collection </h2>
 <Link href={"/products"}>Shop now</Link>
