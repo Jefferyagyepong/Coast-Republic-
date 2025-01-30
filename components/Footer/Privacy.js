ff8ff085af3ade2ca85baa9441115deed4903de6
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 13:40:24 +0000

    Update Privacy.js

--- components/Footer/Privacy.js
+++ components/Footer/Privacy.js
@@ -1,333 +1,122 @@
 function Privacy() {
   return (
-    <div className="ibrid-box-line-law">
+    <div >
       <article>
-           <section> 
-                <em>
-        {" "}
-        <h2>PRIVACY POLICY</h2>
-      </em>
-<br/><br/>
-      <p>
-        This Privacy Policy describes how coast republic (the “Site” or “we”)
-        collects, uses, and discloses your Personal Information when you visit
-        or make a purchase from the Site.
-      </p>
-            </section>
-         </article> <br/><br/>
-         <article> 
-              <section> 
-                  <em>
-        {" "}
-        <h3>Contact</h3>
-      </em>
+  <h3>Effective Date: 20-01-2025</h3>
 
-      <p>
-        After reviewing this policy, if you have additional questions, want more
-        information about our privacy practices, or would like to make a
-        complaint, please contact us by e-mail at info@coastrepublicgh.com.
-      </p>
-                </section>
-          </article><br/><br/>
-              <article> 
-                <section>
-                       <em>
-        {" "}
-        <h3>Collecting Personal Information</h3>
-      </em>
+<p>COAST REPUBLIC INC. ("we," "our," or "us") respects your privacy and is committed to protecting the personal information you provide when using our website and services. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website www.coast-republic.com, use our mobile application, or engage with our services.
 
-      
+By using our platform, you agree to the terms of this Privacy Policy. If you do not agree, please do not use our services.</p>
 
-      <p>
-        When you visit the Site, we collect certain information about your
-        device, your interaction with the Site, and information necessary to
-        process your purchases. We may also collect additional information if
-        you contact us for customer support. In this Privacy Policy, we refer to
-        any information about an identifiable individual (including the
-        information below) as “Personal Information”. See the list below for
-        more information about what Personal Information we collect and why.
-      </p>
+</article>
+<article>
+<h4>1.Information We Collect</h4>
 
-      <p>
-        Device information Purpose of collection: to load the Site accurately
-        for you, and to perform analytics on Site usage to optimize our Site.
-        Source of collection: Collected automatically when you access our Site
-        using cookies, log files, web beacons, tags, or pixels. Disclosure for a
-        business purpose: shared with our processor Shopify Personal Information
-        collected: version of web browser, IP address, time zone, cookie
-        information, what sites or products you view, search terms, and how you
-        interact with the Site
-      </p>
+<p>We collect different types of information when you interact with our e-commerce website:</p>
 
-      <p>
-        Order information Purpose of collection: to provide products or services
-        to you to fulfill our contract, to process your payment information,
-        arrange for shipping, and provide you with invoices and/or order
-        confirmations, communicate with you, screen our orders for potential
-        risk or fraud, and when in line with the preferences you have shared
-        with us, provide you with information or advertising relating to our
-        products or services. Source of collection: collected from you.
-        Disclosure for a business purpose: shared with our processor Paystack.
-        Personal Information collected: name, billing address, shipping address,
-        payment information (including credit card numbers), email address, and
-        phone number.
-      </p>
+<h5>a. Personal Information</h5>
+<p>When you create an account, place an order, or contact us, we may collect:</p>
+<ul>
+  <li>
+    Name
+  </li>
+    <li>
+    Email address
+  </li>
+    <li>
+    Phone number
+  </li>
+    <li>
+    Billing and shipping address
+  </li>
+  
+</ul>
 
-      <p>
-        Customer support information Purpose of collection: to provide customer
-        support. Source of collection: collected from you Disclosure for a
-        business purpose: Personal Information collected: collected from you
-      </p>
-                   </section>
-                </article><br/><br/>
-                 <article> 
-                   <section> 
-                    
-      <em>
-        {" "}
-        <h3>Minors</h3>
-      </em>
+<p>Payment details (processed securely by third-party payment processors)</p>
+</article>
 
-      <p>
-        The Site is not intended for individuals under the age of 18. We do not
-        intentionally collect Personal Information from children. If you are the
-        parent or guardian and believe your child has provided us with Personal
-        Information, please contact us at the address above to request deletion.
-      </p>
-                    </section>
-                  </article><br/><br/>
-                    <article> 
-                        <section> 
-        
-      <em>
-        {" "}
-        <h3>Sharing Personal Information</h3>
-      </em>
-
-      <p>
-        We share your Personal Information with service providers to help us
-        provide our services and fulfill our contracts with you, as described
-        above. For example: We use Google Analytics to help us understand how
-        our customers use the Site. You can read more about how Google uses your
-        Personal Information here:
-        https://www.google.com/intl/en/policies/privacy/. You can also opt-out
-        of Google Analytics here: https://tools.google.com/dlpage/gaoptout. We
-        share information about your use of the Site, your purchases, and your
-        interaction with our ads on other websites with our advertising
-        partners. We collect and share some of this information directly with
-        our advertising partners, and in some cases through the use of cookies
-        or other similar technologies (which you may consent to, depending on
-        your location). We use Shopify Audiences to help us show ads on other
-        websites with our advertising partners to buyers who made purchases with
-        other Shopify merchants and who may also be interested in what we have
-        to offer. We also share information about your use of the Site, your
-        purchases, and the email address associated with your purchases with
-        Shopify Audiences, through which other Shopify merchants may make offers
-        you may be interested in. For more information about how targeted
-        advertising works, you can visit the Network Advertising Initiative’s
-        (“NAI”) educational page at
-        https://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
-        You can opt out of targeted advertising by: [INCLUDE OPT-OUT LINKS FROM
-        WHICHEVER SERVICES BEING USED. COMMON LINKS INCLUDE: FACEBOOK -
-        https://www.facebook.com/settings/?tab=ads GOOGLE -
-        https://www.google.com/settings/ads/anonymous BING -
-        https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads]
-        Additionally, you can opt out of some of these services by visiting the
-        Digital Advertising Alliance’s opt-out portal at:
-        https://optout.aboutads.info/.
-      </p>
-        </section>
-                      </article><br/><br/>
-                  
-         <article>
-             <section> 
-                   <em>
-        {" "}
-        <h3>Using Personal Information</h3>
-      </em>
-
-      <p>
-        We use your personal Information to provide our services to you, which
-        includes: offering products for sale, processing payments, shipping and
-        fulfillment of your order, and keeping you up to date on new products,
-        services, and offers. Lawful basis Pursuant to the General Data
-        Protection Regulation (“GDPR”), if you are a resident of the European
-        Economic Area (“EEA”), we process your personal information under the
-        following lawful bases: Your consent; The performance of the contract
-        between you and the Site; Compliance with our legal obligations; To
-        protect your vital interests; To perform a task carried out in the
-        public interest; For our legitimate interests, which do not override
-        your fundamental rights and freedoms.
-      </p>
-              </section>
-           </article><br/><br/>
-              <article> 
-                   <section> 
-                    
-      <em>
-        <h3>Retention</h3>
-      </em>
-
-      <p>
-        When you place an order through the Site, we will retain your Personal
-        Information for our records unless and until you ask us to erase this
-        information. For more information on your right of erasure, please see
-        the ‘Your rights’ section below.
-      </p>
-                    </section>
-                </article><br/><br/>
-                    <article> 
-                         <section>
-                             <em>
-        <h3>Automatic decision-making</h3>
-      </em>
-
-      <p>
-        If you are a resident of the EEA, you have the right to object to
-        processing based solely on automated decision-making (which includes
-        profiling), when that decision-making has a legal effect on you or
-        otherwise significantly affects you.We DO engage in fully automated
-        decision-making that has a legal or otherwise significant effect using
-        customer data. Our processor Shopify uses limited automated
-        decision-making to prevent fraud that does not have a legal or otherwise
-        significant effect on you. Services that include elements of automated
-        decision-making include: Temporary blacklist of IP addresses associated
-        with repeated failed transactions. This blacklist persists for a small
-        number of hours. Temporary blacklist of credit cards associated with
-        blacklisted IP addresses. This blacklist persists for a small number of
-        days.
-      </p>
-                           </section>
-                      </article><br/><br/>
-                        <article>
-                             <section>
-                               
-      <em>
-        <h3>Selling Personal Information</h3>
-      </em>
-
-      <p>
-        Our Site sells Personal Information, as defined by the California
-        Consumer Privacy Act of 2018 (“CCPA”). [Insert: categories of
-        information sold; IF USING SHOPIFY AUDIENCES: information about your use
-        of the Site, your purchases, and the email address associated with your
-        purchase instructions on how to opt-out of sale; whether your business
-        sells information of minors (under 16) and whether you obtain
-        affirmative authorization; if you provide a financial incentive to sell
-        information, provide information about what that incentive is.]
-      </p>
-                               </section>
-                           </article><br/><br/>
-                <article>
-                   <section> 
-                     
-
-      <em>
-        <h3>Your rights</h3>
-      </em>
-
-      <p>
-        GDPR If you are a resident of the EEA, you have the right to access the
-        Personal Information we hold about you, to port it to a new service, and
-        to ask that your Personal Information be corrected, updated, or erased.
-        If you would like to exercise these rights, please contact us through
-        the contact information above. [OR INSERT ALTERNATIVE INSTRUCTIONS FOR
-        SENDING ACCESS, ERASURE, CORRECTION, AND PORTABILITY REQUESTS] Your
-        Personal Information will be initially processed in Ireland and then
-        will be transferred outside of Europe for storage and further
-        processing, including to Canada and the United States. For more
-        information on how data transfers comply with the GDPR, see Shopify’s
-        GDPR Whitepaper:
-        https://help.shopify.com/en/manual/your-account/privacy/GDPR. CCPA If
-        you are a resident of California, you have the right to access the
-        Personal Information we hold about you (also known as the ‘Right to
-        Know’), to port it to a new service, and to ask that your Personal
-        Information be corrected, updated, or erased. If you would like to
-        exercise these rights, please contact us through the contact information
-        above. If you would like to designate an authorized agent to submit
-        these requests on your behalf, please contact us at the address above.
-      </p>
-                    </section>
-                   </article><br/><br/>
-                    <article> 
-                       <section>
-                         
-
-      <em>
-        {" "}
-        <h3>Cookies</h3>
-      </em>
-
-      <p>
-        A cookie is a small amount of information that’s downloaded to your
-        computer or device when you visit our Site. We use a number of different
-        cookies, including functional, performance, advertising, and social
-        media or content cookies. Cookies make your browsing experience better
-        by allowing the website to remember your actions and preferences (such
-        as login and region selection). This means you don’t have to re-enter
-        this information each time you return to the site or browse from one
-        page to another. Cookies also provide information on how people use the
-        website, for instance whether it’s their first time visiting or if they
-        are a frequent visitor.
-      </p>
-                         </section>
-                      </article><br/><br/>
-                      
-         <article>
-            <section>
-                  <em>
-        {" "}
-        <h3>Do Not Track</h3>
-      </em>
-
-      <p>
-        Please note that because there is no consistent industry understanding
-        of how to respond to “Do Not Track” signals, we do not alter our data
-        collection and usage practices when we detect such a signal from your
-        browser.
-      </p>
-               </section>
-           </article>         <br/><br/>
-            <article>
-               <section> 
-                
-      <em>
-        {" "}
-        <h3>Changes</h3>
-      </em>
-
-      <p>
-        We may update this Privacy Policy from time to time in order to reflect,
-        for example, changes to our practices or for other operational, legal,
-        or regulatory reasons.
-      </p>
-                </section>
-               </article><br/><br/>
-    
-    <article>
-               <section>
-                      <em>
-        {" "}
-        <h3>Complaints</h3>
-      </em>
-
-      <p>
-        As noted above, if you would like to make a complaint, please contact us
-        by e-mail or by mail using the details provided under “Contact” above.
-        If you are not satisfied with our response to your complaint, you have
-        the right to lodge your complaint with the relevant data protection
-        authority. You can contact your local data protection authority, or our
-        supervisory authority here: [Add contact information or website for the
-        data protection authority in your jurisdiction. For example:
-        https://ico.org.uk/make-a-complaint/] Last updated: April 08, 2023
-      </p>
-                </section>
-               </article><br/><br/>
-               <h6 className="footer-tag">
-        &copy; {new Date().getFullYear()} Coast Republic All rights Reserved
-      </h6> <br/>
+<article>
  
-    </div>
+<h5>b. Non-Personal Information</h5>
+<p>We automatically collect data when you visit our site, including:</p>
+
+<ul>
+  <li>
+    IP address
+  </li>
+   <li>
+    Browser type and version
+  </li>
+   <li>
+    Device type
+  </li>
+   <li>
+    Referring website
+  </li>
+   <li>
+    Pages viewed and time spent
+  </li>
+</ul>
+</article>
+
+<article>
+  <h5>c. Cookies and Tracking Technologies</h5>
+<p>We use cookies and similar technologies to improve user experience, analyze site traffic, and personalize content. You can manage cookie preferences through your browser settings.</p>
+
+
+</article>
+
+
+
+
+
+
+
+<article>
+
+<h5>2. How We Use Your Information</h5>
+
+<p>We use your data for the following purposes:</p>
+<ul>
+  <li>
+    To process and fulfill your orders
+  </li>
+   <li>
+    To communicate with you regarding your orders, inquiries, and promotions
+  </li>
+   <li>
+    To improve our website, products, and customer service
+  </li>
+   <li>
+    To prevent fraud and secure transactions
+  </li>
+   <li>    
+To comply with legal obligations
+  </li>
+</ul>
+
+
+
+
+<article>
+
+<ul>
+  <p>
+  </p>
+</ul>
+
+
+
+
+
+
+
+
+       
+      
+  
+   
+   
   );
 }
 export default Privacy;
