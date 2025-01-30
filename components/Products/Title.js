c9a9bb9cfc92fb004c2824da89771d9d8ffd493c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-30 00:53:33 +0000

    Update Title.js

--- components/Products/Title.js
+++ components/Products/Title.js
@@ -3,9 +3,7 @@
 function Title() {
   return (
  <div  className="footer-align-left">
-    <div className="jean-bg">
-        
-     </div>
+  
     
 <h2>Sneakers: Designer pairs in our collection </h2>
 <Link href={"/products"}>Shop now</Link>
