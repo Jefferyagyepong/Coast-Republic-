e70402efe0e23d31d07f6f2abb4dd809706f32e1
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-31 16:34:37 +0000

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { removeFromCart, updateQuantity, clearCart } from "@/redux/cartslice.js";
+import { removeFromCart, updateQuantity, clearCart } from '@/redux/cartslice';
 import { useRouter } from "next/router";
 
 const CartPage = () => {
