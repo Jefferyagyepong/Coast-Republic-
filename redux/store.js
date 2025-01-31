8f6d176b2fafc1939f46a9c6488dac2c5f621ba8
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-31 12:06:15 +0000

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { cartReducer } from '@redux/cart.slice.js';
+import { cartReducer } from '/cart.slice.js';
 
 const reducer = {
  cart: cartReducer,
