b3c7b51c593179941a943a611970e3ebde24e8bc
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-03 15:02:23 +0000

    Update Header.js

--- components/Head/Header.js
+++ components/Head/Header.js
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import CartBasket from "./CartBasket";
+import Menu from "./Menu";
 import Link from "next/link.js";
 
 function Header() {
   return (
     <>
       <header>
-   
+   <div>
           <Link href={"./"}>
             <Image src={"/crlogo.svg"} width={90} height={70} alt="logo" />
           </Link>
+          <Menu />
+          
+    </div>
+    
           <h3>Coast Republic</h3>
 
           <CartBasket />
