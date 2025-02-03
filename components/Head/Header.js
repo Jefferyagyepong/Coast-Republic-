e8ce66d52b6c87a41917fe3795cc9e6716546769
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-03 07:26:33 +0000

    Update Header.js

--- components/Head/Header.js
+++ components/Head/Header.js
@@ -11,7 +11,7 @@ function Header() {
             <Image src={"/crlogo.svg"} width={90} height={70} alt="logo" />
           </Link>
           <h3>Coast Republic</h3>
-           <Link href="/" className="font-bold text-lg">E-Commerce Store</Link>
+           <Link href="/search" >E-Commerce Store</Link>
       <div className="space-x-4">
         <Link href="/search">Search</Link>
       </div>
