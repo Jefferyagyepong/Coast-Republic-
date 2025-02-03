6a7c4b67209c0a45cc673d62d288e2ae45708ad5
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-03 14:53:15 +0000

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -10,12 +10,89 @@ export default function handler(req, res) {
   }
 
   // Dummy product data (Replace with database query)
-  const products = [
-    { id: 1, name: 'Apple iPhone 15' },
-    { id: 2, name: 'Samsung Galaxy S23' },
-    { id: 3, name: 'Google Pixel 8' },
-    { id: 4, name: 'OnePlus 11' },
-  ];
+  const products = 
+[
+  {
+    "slug": " Air Force 1 07 black",
+    "name": " Air Force 1 07 black",
+    "description": "This is a great product.",
+    "price": 19.99,
+    "_id": 1,    
+    "image": "/products/force1a.JPG",    
+     "brand": "Nike"
+    
+  },
+  {
+    "slug": " Reebok Classics",
+    "name": " Reebok Classics",
+    "description": "This is a great product.",
+    "_id": 2,    
+    "image": "/products/reebook1c.JPG",
+    "price": 90.00,
+    "brand": "Reebok"
+     
+  },
+  {
+    "slug": " Air Force 1 07 high black",
+    "name": " Air Force 1 07 high black",
+    "description": "This is a great product.",
+    "_id": 3,
+    "image": "/products/force3a.JPG",
+    "price": 130.00,    
+    "brand": "Nike"
+    
+  },
+  {
+    "slug": " Adidas Campus",
+    "name": " Adidas Campus",
+    "description": "This is a great product.",
+    "_id": 4,
+    "image": "/products/campus1a.JPG",
+    "price": 85.00,    
+    "brand": "Adidas"
+    
+  },
+  {
+    "slug": " Converse Chuck Taylor",
+    "name": " Converse Chuck Taylor",
+    "description": "This is a great product.",
+    "_id": 5,      
+    "image": "/products/chuck1a.JPG",
+    "price": 75.00,    
+    "brand": "Converse"
+    
+  },
+  {
+    "slug": " Timberland boots",
+    "name": " Timberland boots",
+    "description": "This is a great product.",
+    "_id": 6,    
+    "image": "/products/tims1a.JPG",
+    "price": 170.00,
+    "brand": "Timberland"
+  
+  },
+  {
+    "slug": "vans",
+    "name": " vans",
+    "description": "This is a great product.",
+    "_id": 7,
+    "image": "/products/vans1a.WEBP",
+    "price": 80.00,  
+    "brand": "Vans"
+   
+  },
+  {
+    "slug": " Reebok",
+    "name": " Reebok",
+    "description": "This is a great product.",
+    "_id": 8,
+    "image": "/products/reebook1b.WEBP",
+    "price": 120.00,    
+    "brand": "Reebok"
+  
+  }
+];
 
   // Filter products based on search query
   const filteredProducts = products.filter(product =>
