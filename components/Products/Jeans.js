d0b421d5f97ff78193b711ef4a12ffc5f7ad56d7
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-04 21:36:28 +0000

    Update Jeans.js

--- components/Products/Jeans.js
+++ components/Products/Jeans.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function Show() {
   return (
      <div className="footer-align-left">
-        <Image src={"/products/carhartt.JPG"} width={300} height={700} alt="img"/>
+        <Image src={"/products/carhartt.JPG"} width={360} height={700} alt="img"/>
       <h5>Journal: Designer pairs in our collection </h5>
       <Link href={"/products"}>Shop now</Link>
     </div>
