8b6cb2ea8e2fa38b577b1991b66b500ada913ed9
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-05 21:09:50 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Link from 'next/link';
-import styles from "@/sass/ShopPage.module.scss"; // Import CSS file
+import styles from "../sass/ShopPage.module.scss"; // Import CSS file
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
