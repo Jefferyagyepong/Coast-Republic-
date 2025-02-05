453fae38eac6bc68c2ae9952cc93246f95f08e89
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-05 19:49:36 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -1,1 +1,38 @@
-1
+import { useState } from "react";
+import Link from 'next/link';
+import styles from "../styles/ShopPage.module.scss"; // Import CSS file
+
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logo}>My Logo</div>
+        <button
+          className={styles.hamburger}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? "✖" : "☰"}
+        </button>
+        <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="#">About</Link>
+          </li>
+          <li>
+            <Link href="/products/">Shop</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default HamburgerMenu;
+
