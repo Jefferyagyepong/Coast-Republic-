e6c914f62b136168a12c2579ec498a615d8aa21c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-06 20:51:04 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import styles from '../sass/ShopPage.module.css';
+import styles from './sass/Navbar.module.sass';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
