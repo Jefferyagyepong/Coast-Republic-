74d12ee49a84305735032a127d62305e307c6fcc
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-06 11:06:23 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -7,69 +7,26 @@ export default function HamburgerMenu() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="hamburger-menu">
-      <input
-        type="checkbox"
-        id="menu-toggle"
-        className="menu-toggle"
-        checked={open}
-        onChange={() => setOpen(!open)}
-      />
-      <label htmlFor="menu-toggle">
-        <div className="hamburger-icon"></div>
-        <div className="hamburger-icon"></div>
-        <div className="hamburger-icon"></div>
-      </label>
-      <div className="menu">
-        <Link href="/">Home</Link>
-        <Link href="/products/">Shop</Link>
-        <Link href="#">About</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
+    <nav>
       
-      <style jsx>{`
-        .hamburger-menu {
-  position: relative;
-  display: inline-block;
-}
+        <input type="checkbox" id="menu-toggle" />
+        
+        
+        <label for="menu-toggle" className="menu-icon">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </label>
 
-.hamburger-icon {
-  width: 30px;
-  height: 3px;
-  background-color: #333;
-  margin: 6px 0;
-  transition: 0.4s;
-}
-
-.menu-toggle {
-  display: none;
-}
-
-.menu {
-  display: none;
-  position: absolute;
-  top: 40px;
-  left: 0;
-  background: white;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  width: 150px;
-  padding: 10px;
-}
-
-.menu a {
-  display: block;
-  text-decoration: none;
-  color: black;
-  padding: 10px;
-}
-
-.menu-toggle:checked + .menu {
-  display: block;
-}
+        
+        <ul className="nav-links">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="#">About</Link></li>
+            <li><Link href="/products/">Shop Now</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
+        </ul>
+    </nav>
 
 
-  
-      `}</style>
-    </div>
   );
 }
