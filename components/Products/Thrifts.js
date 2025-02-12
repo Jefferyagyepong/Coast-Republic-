47411abfe9a2040ea418621abf01144ecc3f0174
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-12 09:45:14 +0000

    Update Thrifts.js

--- components/Products/Thrifts.js
+++ components/Products/Thrifts.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function All() {
   return (
     <div className="footer-align-left">
-      <Image src={"https://images.unsplash.com/photo-1600269452339-56203889e71c?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="img" width={380} height={800} />
+      <Image src={"https://images.unsplash.com/photo-1584386450584-c6d766612a45?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fE5pa2UlMjBBaXIlMjBzbmVha2VyfGVufDB8fDB8fHww"} alt="img" width={380} height={800} />
       <h5>Sneakers: Designer pairs in our collection </h5>
       <Link href={"/products"}>Shop now</Link>
     </div>
