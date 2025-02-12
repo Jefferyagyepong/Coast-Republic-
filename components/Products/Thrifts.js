046796d3d8ff3e72456abcd9bb56c1c44e567b3d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-12 09:41:49 +0000

    Update Thrifts.js

--- components/Products/Thrifts.js
+++ components/Products/Thrifts.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function All() {
   return (
     <div className="footer-align-left">
-      <Image src={"/products/force3a.JPG"} alt="img" width={360} height={500} />
+      <Image src={"https://images.unsplash.com/photo-1600269452339-56203889e71c?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="img" width={380} height={800} />
       <h5>Sneakers: Designer pairs in our collection </h5>
       <Link href={"/products"}>Shop now</Link>
     </div>
