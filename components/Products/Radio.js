184e3e2d087c523fa9d3c6a6505c08ed4c4fb99f
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-12 10:41:35 +0000

    Update Radio.js

--- components/Products/Radio.js
+++ components/Products/Radio.js
@@ -12,19 +12,19 @@ const CarouselRadio = () => {
         <input type="radio" name="radio-btn" id="slide3" />
 
         <div className="slideRadio first">
-        <div className="footer-align-left"> 
+      
         <h4>GET THE COAST REPUBLIC APP </h4>
         <p>Download and enjoy our app anytime, anywhere for IOS and Android devices.</p>
-
-      <div className="ibrid-flex">
-          <Image src={"/apple.svg"} alt="apple" width={40} height={30} />
-          <Image src={"/android.svg"} alt="android" width={40} height={30} />
+        <div className="ibrid-flex">
+        <Image src={"/apple.svg"} alt="apple" width={40} height={30} />
+        <Image src={"/android.svg"} alt="android" width={40} height={30} />
         </div>
-      </div>
         </div>
+        
         <div className="slideRadio">
           <Image src="https://via.placeholder.com/800x400/7f7fff/fff?text=Slide+2" alt="Slide 2" />
         </div>
+        
         <div className="slideRadio">
           <Image src="https://via.placeholder.com/800x400/7fff7f/fff?text=Slide+3" alt="Slide 3" />
         </div>
