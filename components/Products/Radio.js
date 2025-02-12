51f2cd68d92e175de2e5c90ffdd365e6b22d1822
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-12 10:48:53 +0000

    Update Radio.js

--- components/Products/Radio.js
+++ components/Products/Radio.js
@@ -4,25 +4,25 @@ import Image from "next/image";
 
 const CarouselRadio = () => {
   return (
-    <div className="carouselRadio">
+    <div className="carousel">
       {/* Slides */}
-      <div className="slidesRadio">
+      <div className="slides">
         <input type="radio" name="radio-btn" id="slide1" defaultChecked />
         <input type="radio" name="radio-btn" id="slide2" />
         <input type="radio" name="radio-btn" id="slide3" />
 
-        <div className="slideRadio first">      
+        <div className="slide first">      
         <h4>GET THE COAST REPUBLIC APP </h4>
         <p>Download and enjoy our app anytime, anywhere for IOS and Android devices.</p>        
         <Image src={"/apple.svg"} alt="apple" width={40} height={30} />
         <Image src={"/android.svg"} alt="android" width={40} height={30} />
         </div>
         
-        <div className="slideRadio">
+        <div className="slide">
           <Image src="https://via.placeholder.com/800x400/7f7fff/fff?text=Slide+2" alt="Slide 2" />
         </div>
         
-        <div className="slideRadio">
+        <div className="slide">
           <Image src="https://via.placeholder.com/800x400/7fff7f/fff?text=Slide+3" alt="Slide 3" />
         </div>
       </div>
