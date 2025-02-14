5de9e17d8d06edd3341b372281a2dd3738bb5e46
Jefferyagyepong <agyepong9jeff@icloud.com>
2025-02-14 11:13:31 +0000

    init

--- components/Head/Header.js
+++ components/Head/Header.js
@@ -6,12 +6,27 @@ function Header() {
   return (
     <>
       <header>
-                 
-        <Link href={"./"}>            
-       
-        <h5>Coast Republic</h5>
-        </Link>                                      
-        <CartBasket />    
+        <nav>
+          <ul>
+            <li>
+              <Link href={"/"}></Link>
+            </li>
+          </ul>
+          <label for="menu" tabIndex="0">=</label>
+          <input type="checkbox" id="menu" />
+          <ul>
+            <li>
+              <Link href={"/contact"}></Link>
+              <Link href={"/privacy"}></Link>
+              <Link href={"/ters"}></Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Link href={"./"}>
+          <h5>Coast Republic</h5>
+        </Link>
+        <CartBasket />
       </header>
     </>
   );
