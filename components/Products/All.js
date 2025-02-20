d5aa966666dfb046a4f3271b9befa19fbaa39250
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-20 23:04:17 +0000

    Update All.js

--- components/Products/All.js
+++ components/Products/All.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function All() {
   return (
     <div className="footer-align-left">
-      <Image src={"/basqu.jpg"} alt="img" width={400} height={700} />
+      <Image src={"/basqu.jpg"} alt="img" width={370} height={700} />
       <h5>How to dress for your age</h5><br/>
       <Link href={"/products"}>Read</Link>
     </div>
