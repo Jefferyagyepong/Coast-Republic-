62c98014aee1e84366c5faae3afb175f71b41763
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-21 22:07:27 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -3,14 +3,14 @@ function Menu() {
   return (
   <div id="navbar">
   <div id="navbar-content">
-    <span class="text text-bold">M😋ji</span>
+    <span className="text text-bold">=</span>
     <div id="defocus"></div>
     <div id="navbar-link-area">
       <div id="menu-bg"></div>
       <div id="menu-btn">
-        <span class="text text-bold"></span>
+        <span className="text text-bold"></span>
       </div>
-      <div class="text navbar-links">
+      <div className="text navbar-links">
         <Link href="/">Home</Link>
         <Link href="/b">Blog</Link>
         <Link href="/a">About</Link>
