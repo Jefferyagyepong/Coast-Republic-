fabf0640be7009f1ab0f39a676a60245809940ed
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-25 14:11:27 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -2,18 +2,15 @@ import Link from "next/link";
 function Menu() {
   return (
 <>
-      
-        <input className="side-menu" type="checkbox" id="side-menu"/>
-        <label className="hamb" for="side-menu"><span className="hamb-line"></span></label>
-       
-        <nav className="nav">
-            <ul className="menu">
-                <li><Link href="#">Shop</Link></li>
-                <li><Link href="#">Delivery Info</Link> </li>
-                <li><Link href="#">About Us</Link></li>
-        <li><Link href="#">Privacy</Link></li>
-            </ul>
-        </nav>
+    <Link href="" className="logo">CSS Nav</Link>
+  <input className="menu-btn" type="checkbox" id="menu-btn" />
+  <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
+  <ul class="menu">
+    <li><Link href="#work">Our Work</Link></li>
+    <li><Link href="#about">About</Link></li>
+    <li><Link href="#careers">Careers</Link></li>
+    <li><Link href="#contact">Contact</Link></li>
+  </ul>
     </>
   );
 }
