20669bfac4646018ce2d4310648fa5fdf5202cce
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-25 09:06:36 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -1,37 +1,19 @@
 import Link from "next/link";
 function Menu() {
   return (
-<nav role="navigation">
-  <div id="menuToggle">
-  
-    <input type="checkbox" id="menuCheckbox" />
-
-    <span></span>
-    <span></span>
-    <span></span>
-
-    <ul id="menu">
- 
-      <li>
-        <Link href="#">
-          <label for="menuCheckbox" onclick="this.parentNode.click();">Home</label>
-        </Link>
-      </li>
-      <li>
-        <Link href="#about">
-          <label for="menuCheckbox" onclick="this.parentNode.click();">About</label>
-        </Link>
-      </li>
-    
-      <li><label for="menuCheckbox"><Link>Info</Link></label></li>
-      <li><label for="menuCheckbox"><Link>Contact</Link></label></li>
-
-      <li>
-        <Link href="https://erikterwan.com/" target="_blank">Show me more</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
+ <!-- Logo -->
+      
+        <input className="side-menu" type="checkbox" id="side-menu"/>
+        <label className="hamb" for="side-menu"><span className="hamb-line"></span></label>
+        <!-- Menu -->
+        <nav className="nav">
+            <ul className="menu">
+                <li><Link href="#">Shop</Link></li>
+                <li><Link href="#">Delivery Info</Link> </li>
+                <li><Link href="#">About Us</Link></li>
+        <li><Link href="#">Privacy</Link></li>
+            </ul>
+        </nav
   );
 }
 export default Menu;
