c6fbee661f58f8e437d248d8f01cc02edbff42b4
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-02-25 17:32:38 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -9,11 +9,11 @@ function Menu() {
     <div className='menu-button'></div>
   </label>
      <ul className="menu">
-      <li>One</li>
-      <li>Two</li>
-      <li>Three</li>
-      <li>Four</li>
-      <li>Five</li>
+      <li><Link href="/">Home</Link></li>
+         <li><Link href="/products/">Shop</Link></li>
+       <li><Link href="/privacy">Privacy</Link></li>
+       <li><Link href="/delivery">Track Order</Link></li>
+       <li><Link href="/about">About Us</Link></li>
     </ul>
   </section>
     </>
