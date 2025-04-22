3ed3caf23c7662a3964731eca3d56c615bec0c5c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-04-22 13:27:35 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -9,8 +9,7 @@ export default function HamburgerMenu() {
         <span className="line line1"></span>
         <span className="line line2"></span>
         <span className="line line3"></span>
-      </label>
-            <div className="menu-items">
+        <div className="menu-items">
               <ul>
                 <li>
                   <Link id="menu-link" href="/">Home</Link>
@@ -29,6 +28,8 @@ export default function HamburgerMenu() {
                 </li>
               </ul>
             </div>
+      </label>
+            
     </div>
   );
 }
