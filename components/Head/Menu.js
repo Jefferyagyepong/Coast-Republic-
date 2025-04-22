c2bf6a79205aa9049bc2882b5527c0dfe5e93ac5
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-04-22 13:33:33 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -1,35 +1,23 @@
 // components/HamburgerMenu.jsx
 import Link from 'next/link';
+// components/HamburgerMenu.jsx
+
 
 export default function HamburgerMenu() {
   return (
-    <div className="hamburger">
-      <input type="checkbox" className="checkbox" id="hamburger-toggle" />
+    <nav className="nav">
+      <input type="checkbox" className= "checkbox" id="hamburger-toggle" />
       <label htmlFor="hamburger-toggle" className="label">
         <span className="line line1"></span>
         <span className="line line2"></span>
         <span className="line line3"></span>
-        <div className="menu-items">
-              <ul>
-                <li>
-                  <Link id="menu-link" href="/">Home</Link>
-                </li>
-                <li>
-                  <Link href="/about">About</Link>
-                </li>
-                <li>
-                  <Link href="/products/">Shop</Link>
-                </li>
-                <li>
-                  <Link href="/privacy">Privacy</Link>
-                </li>
-                <li>
-                  <Link href="/contact">contact</Link>
-                </li>
-              </ul>
-            </div>
       </label>
-            
-    </div>
+      <ul className="menu">
+        <li><Link href="/" className="link">Home</Link></li>
+        <li><Link href="/about" className="link">About</Link></li>
+        <li><Link href="/services" className="link">Services</Link></li>
+        <li><Link href="/contact" className="link">Contact</Link></li>
+      </ul>
+    </nav>
   );
 }
