3374d11295b462814c7f67a87d255828736481ce
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-04-24 22:38:55 +0000

    Update Title.js

--- components/Products/Title.js
+++ components/Products/Title.js
@@ -13,8 +13,7 @@ function Title() {
         height={700}
       />
       <h5>Sneakers: Designer pairs in our collection </h5>
-      <br />
-      <br />
+        
       <Link href={"/products"}>Shop now</Link>
     </div>
   );
