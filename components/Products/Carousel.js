4e1aff960c232b3c526fc3486985cdfe61c65d98
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-04-28 23:04:13 +0000

    Update Carousel.js

--- components/Products/Carousel.js
+++ components/Products/Carousel.js
@@ -13,7 +13,7 @@ export default function Carousel({ images }) {
       <style jsx>{`
         .carousel {
           width: 100%;
-          max-width: 300px;
+          max-width: 600px;
           background:hsl(0%, 0%, 92%);
           margin: 0 auto;
           overflow: hidden;
