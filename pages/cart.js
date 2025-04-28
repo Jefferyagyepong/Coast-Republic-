a13dfede750bd4409f52f2b6235318b9cc6bd167
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-04-28 21:34:54 +0000

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -150,7 +150,7 @@ const CartPage = () => {
         total={orderData.total}
         />
         
-        <Link href={"/checkout"}><h2>SECURE CHECKOUT</h2></Link>
+        <Link className="text-align-center" href={"/checkout"}><h2>SECURE CHECKOUT</h2></Link>
         
       </main>
     </>
