9e0fe912f9c1928eb75d73863b6615f64258ad7b
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-02 10:38:57 +0000

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -3,10 +3,86 @@ import { useState } from 'react';
 
 // Sample JSON data (replace with your own data or fetch from an API)
 const productsData = [
-  { id: 1, name: 'Laptop', category: 'Electronics', price: 999.99 },
-  { id: 2, name: 'T-Shirt', category: 'Clothing', price: 19.99 },
-  { id: 3, name: 'Headphones', category: 'Electronics', price: 59.99 },
-  { id: 4, name: 'Jeans', category: 'Clothing', price: 49.99 },
+{
+    "_id": 1,
+    "slug": "Air Force 1 07 black",
+    "name": "Air Force 1 07 black",       
+    "image": "/products/force1a.JPG",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 109.99
+
+  },
+  {
+    "_id": 2,
+    "slug": "Reebok classics",
+    "name": "Reebok classics",     
+    "image": "/products/reebook1c.JPG",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 109.99
+
+  },
+  {
+    "_id": 3,
+    "slug": "Air Force 1 07 high black",
+    "name": "Air Force 1 07 high black",
+    "image": "/products/force3a.JPG",
+     "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 130.00
+
+  },
+  {
+    "_id": 4,
+    "slug": " Adidas Campus",
+    "name": " Adidas Campus",
+    "image": "/products/campus1a.JPG",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 185.00
+ 
+  },
+  {
+    "_id": 5,
+    "slug": "Converse Chuck Taylor",
+    "name": "Converse Chuck Taylor",
+    "image": "/products/chuck1a.JPG",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 75.00
+
+  },
+  {
+    "_id": 6,
+    "slug": "Timberland Boots",
+    "name": "Timberland Boots",
+    "image": "/products/tims1a.JPG",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 275.00
+
+  },
+  {
+    "_id": 7,
+    "slug": "vans",
+    "name": "vans",
+    "image": "/products/vans1a.WEBP",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 80.00
+
+  },
+  {
+    "_id": 8,
+    "slug": "Reebok",
+    "name": "Reebok",
+    "image": "/products/reebook1b.WEBP",
+    "category": "Sneaker",
+    "description": "Lorem ipsum dalot gunt Dreh",
+    "price": 120.00
+
+  }
 ];
 
 export default function ProductList() {
