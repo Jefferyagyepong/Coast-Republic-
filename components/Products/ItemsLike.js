f4babcf77fa14a8cd669d50b080a1ac22d3e1e8c
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-03 03:21:11 +0000

    Update ItemsLike.js

--- components/Products/ItemsLike.js
+++ components/Products/ItemsLike.js
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 
 const items = [
-  { id: 1, name: 'Item 1', price: '$10', image: '/images/item1.jpg' },
+  { id: 1, name: 'Item 1', price: '$10', image: '/thugga.jpg' },
   { id: 2, name: 'Item 2', price: '$20', image: '/images/item2.jpg' },
   { id: 3, name: 'Item 3', price: '$30', image: '/images/item3.jpg' },
 ];
