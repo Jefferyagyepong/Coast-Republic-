94e850ff21a62de1c897334a7c78345c8ebaef73
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-04 08:29:39 +0000

    Update ItemsLike.js

--- components/Products/ItemsLike.js
+++ components/Products/ItemsLike.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const items = [
-  { id: 1, name: 'Young Thug Tee', price: 'GHS 300', image: '/thugga.jpg' },
-  { id: 2, name: 'Nike Air Force 1 Tripple Black', price: 'GHS 900',  image: '/products/force1c.JPG'},
-  { id: 3, name: 'Nike Air Force 1 White', price: 'GHS 750', image: '/products/force2a.JPG' },
+  { id: 1, name: 'Young Thug Tee', price: 'GH₵ 300', image: '/thugga.jpg' },
+  { id: 2, name: 'Nike Air Force 1 Tripple Black', price: 'GH₵ 900',  image: '/products/force1c.JPG'},
+  { id: 3, name: 'Nike Air Force 1 White', price: 'GH₵ 750', image: '/products/force2a.JPG' },
 ];
 
 export default function ItemsYouMayLike() {
@@ -15,9 +15,9 @@ export default function ItemsYouMayLike() {
       <div className="items-grid">
         {items.map((item) => (
           <div key={item.id} className="item-card">
-            <Image src={item.image} alt={item.name} className="item-image"  width={200} height={150}/>
-            <h3>{item.name}</h3>
-            <p>{item.price}</p>
+            <Image src={item.image} alt={item.name} className="item-image"  width={150} height={100}/>
+           <br /> <h3>{item.name}</h3><br />
+            <p>{item.price}</p><br />
             
               <Link href={"/products"}>           
                  View Details               
