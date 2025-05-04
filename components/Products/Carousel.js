c47970304d41f7c8f37f76d7e5271ba8b3e1b17a
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-04 11:10:38 +0000

    Update Carousel.js

--- components/Products/Carousel.js
+++ components/Products/Carousel.js
@@ -6,7 +6,7 @@ export default function Carousel({ images }) {
       <div className="carousel-inner">
         {images.map((src, index) => (
           <div key={index} className="carousel-item">
-            <Image src={src} alt={`Slide ${index + 1}`} width={200} height={150} />
+            <Image src={src} alt={`Slide ${index + 1}`} width={250} height={200} />
           </div>
         ))}
       </div>
