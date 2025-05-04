61551ffd8506ced2a7b9de6fb7f690fe91e61232
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-04 13:51:19 +0000

    Update Collection.js

--- components/Products/Collection.js
+++ components/Products/Collection.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Collection() {
   return (
     <div className="footer-align-left">
-     <Image src={"https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="img" width={380} height={700} />    
-      <h5>Tees: Designer pairs in our collection </h5><br/>
-      <Link href={"/products"}>Shop now</Link>
+     <Image src={"https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="img" width={380} height={700} />    
+      <h3>NECCESITIES | Blog </h3>
+      <Link href={"/products"}>Read</Link>
     </div>
   );
 }
