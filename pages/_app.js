caaf6b97edfd6177287ca1f91f319228c2904465
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-06 13:17:03 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import "/sass/main.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import { CartProvider } from '../context/CartContext';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
     </Provider>
   );
 }
