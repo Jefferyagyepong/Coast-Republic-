7572cb1fb5e241a3b9ac825d8580128b75259073
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-06 22:53:20 +0000

    Delete redux directory

--- redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import  cartReducer from '@/cart.slice.js';
-
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
