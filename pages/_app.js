7e28842e72bec64480aeee1e167872b14bd184f7
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-06 13:15:47 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,15 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
-import { store } from "../redux/store";
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
+import { store } from '../store';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
 }
+
+export default MyApp;
