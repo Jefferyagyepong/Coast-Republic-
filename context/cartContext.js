984218e6187f320421ad4ece0a4b73af3442209a
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-10 10:14:06 +0000

    Update cartContext.js

--- context/cartContext.js
+++ context/cartContext.js
@@ -1,7 +1,7 @@
 
 import { createContext, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToCart, removeFromCart } from '../store/cartSlice';
+import { addToCart, removeFromCart } from '../redux/cart.slice';
 
 const CartContext = createContext();
 
