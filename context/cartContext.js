de407ef4455fe942619d61441c9ac45463a89ba7
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-05-13 21:31:57 +0000

    Update cartContext.js

--- context/cartContext.js
+++ context/cartContext.js
@@ -1,27 +1,30 @@
-
 import { createContext, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToCart, removeFromCart } from '../redux/cart.slice';
+import { addToCart, removeFromCart, updateQuantity } from '../store/cartSlice';
 
-const CartContext = createContext();
+const cartContext = createContext();
 
-export function CartProvider({ children }) {
+export const CartProvider = ({ children }) => {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.items);
 
-  const addItem = (item) => {
-    dispatch(addToCart(item));
-  };
+  const addItem = (item) => dispatch(addToCart(item));
+  const removeItem = (id) => dispatch(removeFromCart(id));
+  const updateItemQuantity = (id, quantity) =>
+    dispatch(updateQuantity({ id, quantity }));
 
-  const removeItem = (id) => {
-    dispatch(removeFromCart(id));
-  };
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 
   return (
-    <CartContext.Provider value={{ cartItems, addItem, removeItem }}>
+    <CartContext.Provider
+      value={{ cartItems, addItem, removeItem, updateItemQuantity, totalPrice }}
+    >
       {children}
     </CartContext.Provider>
   );
-}
+};
 
-export const useCart = () => useContext(CartContext);
+export const useCart = () => useContext(cartContext);
