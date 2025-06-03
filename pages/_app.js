575fbdd48640ba325176b9fcd41a17cf36c7e9f2
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-06-03 17:06:41 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ config.autoAddCss = false;
 
 
 import { CartProvider } from "../context/cartContext";
-import { Provider } from 'react-redux';
-import { store } from "../redux/store";
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+     <CartProvider>
       <Component {...pageProps} />
-    </Provider>
+    </CartProvider>
   );
 }
 
