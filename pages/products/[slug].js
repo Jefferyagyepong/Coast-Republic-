2be54780d90e0c4d97a42fe7b6e874db906cc6c1
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-06-15 19:04:59 +0000

    Update [slug].js

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import fs from 'fs';
 import path from 'path';
 import Image from 'next/image';
-import { useCart } from "../context/cartContext";
+import { useCart } from "@/context/cartContext";
 
 
 // This function generates the paths for each product based on the slugs.
