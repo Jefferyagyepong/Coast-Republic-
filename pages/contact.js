c6e8cc4303efb11b5bb494154e241202d332582e
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-06-24 14:14:42 +0000

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -67,9 +67,7 @@ const Contact = () => {
       <div className="sticky">
         <Toast />
         <Header />
-        </div>
-      
-     <div className="contact-container">    
+        </div> 
         <h2>Contact Us</h2>
         <p>
          Have questions? We are here to help! Fill out the form below, and we will get back to you.
