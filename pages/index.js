798ee9b604e75450b54537bc579c301aa67a193a
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-08-10 18:00:21 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,7 @@ const Home = () => {
         />
         <meta
           name="twitter:image"
-          content="https://images.unsplash.com/photo-1622445272461-c6580cab8755?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
+          content="https://imgur.com/a/Yfw9KlD" />
         <meta property="og:title" content="Coast Republic inc" />
         <meta
           property="og:description"
