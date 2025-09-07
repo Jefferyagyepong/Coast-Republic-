54beda1a0fcc8c39fdcabcd9f4301fa41dde0801
Jefferyagyepong <agyepong9jeff@icloud.com>
2025-09-07 10:55:18 +0000

    init sass

---
+++ pages/products/page.js
@@ -0,0 +1,33 @@
+/* eslint-disable react/react-in-jsx-scope */
+"use client";
+
+import { useState, useEffect } from "react";
+import Product from "@/components/Product";
+
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch("ttps://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">E-Commerce Store</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {products.map(product => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+        <div className="mt-8"></div>
+      </div>
+    </>
+  );
+}
