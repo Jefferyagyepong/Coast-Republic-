3079b3b7e590ca84fcb83693303f6b266b3391ca
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-08 22:33:07 +0000

    Update Carousel.js

--- components/Parts/Carousel.js
+++ components/Parts/Carousel.js
@@ -2,35 +2,35 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 // components/Carousel.js
-import styles from './Carousel.module.css';
+
 
 const Carousel = ({ items }) => {
     return (
-        <div className={styles.carouselContainer}>
-            <div className={styles.carousel}>
-                <div className={styles.carouselTrack}>
+        <div className="carouselContainer">
+            <div className="carousel">
+                <div className="carouselTrack">
                     {items.map((item, index) => (
-                        <div key={index} className={styles.carouselItem}>
-                            <img src={item.image} alt={item.brand} className={styles.carouselImage} />
-                            <div className={styles.carouselInfo}>
-                                <div className={styles.carouselBrand}>{item.brand}</div>
+                        <div key={index} className="carouselItem">
+                            <Image src={item.image} alt={item.brand} className="carouselImage" />
+                            <div className="carouselInfo">
+                                <div className="carouselBrand">{item.brand}</div>
                             </div>
                         </div>
                     ))}
                     {/* Duplicate items for seamless looping (optional for infinite scroll) */}
                     {items.map((item, index) => (
-                        <div key={index + items.length} className={styles.carouselItem}>
-                            <img src={item.image} alt={item.brand} className={styles.carouselImage} />
-                            <div className={styles.carouselInfo}>
-                                <div className={styles.carouselBrand}>{item.brand}</div>
+                        <div key={index + items.length} className="carouselItem">
+                            <Image src={item.image} alt={item.brand} className="carouselImage" />
+                            <div className="carouselInfo">
+                                <div className="carouselBrand">{item.brand}</div>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
             {/* Optional navigation buttons (CSS-styled) */}
-            <button className={styles.prevButton}>❮</button>
-            <button className={styles.nextButton}>❯</button>
+            <button className="prevButton">❮</button>
+            <button className="nextButton">❯</button>
         </div>
     );
 };
