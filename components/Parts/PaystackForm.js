aeb5fe8801143af90240505beb8d3540f54cd68a
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-09 11:31:50 +0000

    init

--- components/Parts/PaystackForm.js
+++ components/Parts/PaystackForm.js
@@ -1,6 +1,5 @@
 // components/PaystackForm.js
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 
 const PaystackForm = () => {
