5f59d951b9a2d317bf5898079032299c205574ae
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-09 11:02:04 +0000

    init

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script>"https://js.paystack.co/v1/inline.js"</script>
+      </Head>
        
         
       <body>
