c2ed7971f62204108d5ffe40ad6b8ed56bb8f232
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-11 16:28:48 +0000

    Update index.js

--- pages/products/index.js
+++ pages/products/index.js
@@ -4,7 +4,6 @@
 import { useState } from "react";
 import Header from "@/components/Head/Header";
 import Footer from "@/components/Footer/Footer";
-import Toast from "@/components/Head/Toast";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
