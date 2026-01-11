b663c1c5574ee3f444efafd14bc490dea278ac90
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-11 21:14:28 +0000

    Update Collection.js

--- components/Parts/Collection.js
+++ components/Parts/Collection.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Collection() {
   return (
-    <div className="footer-align-left">
+    <div>
       <Image src= {"/GYAMFUA.svg"} alt="img" width={1200}  height={400}/>
       <h3>NECCESITIES | BLOG </h3>
       <Link href={"/products"}>Read</Link>
