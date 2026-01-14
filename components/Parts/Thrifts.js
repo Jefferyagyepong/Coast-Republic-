df7ec52a45981fe0c669774b285407cb0fd777a1
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-14 22:56:18 +0000

    Update Thrifts.js

--- components/Parts/Thrifts.js
+++ components/Parts/Thrifts.js
@@ -9,7 +9,7 @@ import Image from "next/image";
 function All() {
   return (
     <div>
-      <Image src={"/wrangler.svg"} alt="img" width={1200} height={400} />
+      <Image src={"/IMG_4339.png"} alt="img" width={1200} height={400} />
       <h3>Jeans</h3>
       <Link href={"/products"}>Shop now</Link>
     </div>
