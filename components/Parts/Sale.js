e948817af22fcbb1761c81e0994d784032adddae
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-15 20:35:26 +0000

    Update Sale.js

--- components/Parts/Sale.js
+++ components/Parts/Sale.js
@@ -22,7 +22,7 @@ export default function ImageSlider({ images = [], interval = 4000 }) {
 
   return (
     <div
-      className="relative w-full max-w-4xl mx-auto overflow-hidden rounded-xl shadow-xl group"
+      className="card-wrapper"
       onMouseEnter={() => setPaused(true)}
       onMouseLeave={() => setPaused(false)}
     >
