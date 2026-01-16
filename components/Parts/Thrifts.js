cb35d4a70968e0c73d7dd5d20a8b8d60704886cd
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-16 07:19:41 +0000

    Update Thrifts.js

--- components/Parts/Thrifts.js
+++ components/Parts/Thrifts.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 function All() {
   return (
-    <div className=background-container-women>
+    <div className="background-container-women">
       <h3>Tees</h3>
       <Link href={"/products"}>Shop now</Link>
     </div>
