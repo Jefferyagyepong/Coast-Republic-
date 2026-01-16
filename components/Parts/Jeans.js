8dd8c7abff366a8fb31e5a55324811a0f99d859e
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-16 07:00:14 +0000

    Update Jeans.js

--- components/Parts/Jeans.js
+++ components/Parts/Jeans.js
@@ -1,16 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 import Link from "next/link";
-import Image from "next/image";
+
 
 function Show() {
   return (
-    <div>
-      <Image
-        src={"/IMG_4343.png"}
-        alt="img"
-        width={1200}
-        height={400}
-      />
+    <div className="background-container">
+   
       <h3>DESIGNER PAIRS IN OUR COLLECTION</h3>
 
       <Link href={"/products"}>Shop now</Link>
