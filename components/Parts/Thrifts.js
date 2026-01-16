c410706a6a6accd30954340181c869f39b4ccf7d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-16 07:16:07 +0000

    Update Thrifts.js

--- components/Parts/Thrifts.js
+++ components/Parts/Thrifts.js
@@ -1,16 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-
-
-
 import Link from "next/link";
 import Image from "next/image";
 
 function All() {
   return (
-    <div>
-      <Image src={"/IMG_4339.png"} alt="img" width={1200} height={400} />
-      <h3>Jeans</h3>
+    <div className=background-container-women>
+      <h3>Tees</h3>
       <Link href={"/products"}>Shop now</Link>
     </div>
   );
