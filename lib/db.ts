ae4ca46f043b9ade1b59abe1e11ddd272f2f8622
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-25 18:02:41 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,12 @@
+// lib/db.ts   (or src/lib/db.ts)
 import { PrismaClient } from '@prisma/client'
-const globalForPrisma = globalThis as unknown as {
- prisma: PrismaClient | undefined
-}
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+  })
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
