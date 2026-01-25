72b220c733fb2c2ff91288a8a4ef3a1944faa2fe
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-01-25 18:37:55 +0000

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../../lib/db'
+import { prisma } from '@lib/db'
 import { ProductWithVariantsAndImages } from '@/types/product'
 export async function getFeaturedProducts() {
  return prisma.products.findMany({
