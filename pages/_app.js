c1176c2d868dd9876746d919f5d96db545cf0284
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-04-24 02:06:50 +0000

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,24 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/react-in-jsx-scope */
-
-import  "../sass/main.scss";
-
-
-import React from "react";
-import Header from "@/components/Head/Navbar";
-
 // pages/_app.js
 
+import '../styles/globals.css'; // or '../sass/main.scss' — pick one
+
+import React from 'react';
 import { CartProvider } from '@/context/CartContext';
-
-import Header from '@/components/layout/Header';     // ← your header with cart icon
-
-// import Footer from '@/components/layout/Footer'; // ← optional
-
-import '../styles/globals.css';                      // ← your global styles (tailwind, etc.)
+import Header from '@/components/layout/Header';
+// import Footer from '@/components/layout/Footer';
 
 function MyApp({ Component, pageProps }) {
-
   return (
-<CartProvider>
-<div className="min-h-screen flex flex-col antialiased">
-<Header />
-<main className="flex-grow">
-<Component {...pageProps} />
-</main>
-
-        {/* <Footer /> ← uncomment if you have a footer */}
-</div>
-</CartProvider>
-
+    <CartProvider>
+      <div className="min-h-screen flex flex-col antialiased">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </CartProvider>
   );
-
 }
 
 export default MyApp;
- 
