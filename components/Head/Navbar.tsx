67e5a8d91e76c14cb22d807c6ff3ae350f4b7fd0
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-08-19 15:25:28 +0000

    head updates

--- components/Head/Navbar.tsx
+++ components/Head/Navbar.tsx
@@ -1,26 +1,21 @@
-"use client"
-import Link from 'next/link'
-import { ShoppingCart } from 'lucide-react'
-import { useCart } from '../../context/CartContext'
-import React from 'react'
-export default function Header() {
+/* eslint-disable react/react-in-jsx-scope */
+"use client";
 
- const { itemCount } = useCart()
- return (
-<>
-<Link href="/products" className="text-gray-700 hover:text-gray-900 font-medium">
-             Products
-</Link>
-<Link href="/cart" className="relative text-gray-800 hover:text-gray-900">
-<ShoppingCart size={26} strokeWidth={1.8} />
-             {itemCount > 0 && (
-<span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full min-w-[20px] h-5 flex items-center justify-center px-1.5">
-                 {itemCount > 99 ? '99+' : itemCount}
-</span>
-             )}
-</Link>
-</>
+import { useCart } from "@/context/CartContext";
+import Link from "next/link";
 
 
- )
+export default function Navbar() {
+  const { getCartCount } = useCart();
+
+  return (
+    
+         
+        <Link  className="cart" href={"#"}>
+             Cart ({getCartCount()})
+        </Link>
+       
+      
+      
+  );
 }
