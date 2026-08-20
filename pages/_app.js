8b9f5841396f7b52c6b524561e7cf5c37272d9f0
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-08-20 21:26:20 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 
+import "@/sass/main.scss"; // adjust to match your actual global stylesheet import
+import { CartProvider } from "@/context/CartContext";
 
-import  "../sass/main.scss";
-
-import { CartProvider } from "../context/CartContext";
-import ProductList from "@/components/Product";
-import Cart from "@/components/Cart";
-import CartOverlay from "@/components/CartOverlay";
-
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
       <Component {...pageProps} />
-      <Cart />
-      <CartOverlay />
-      <ProductList />
     </CartProvider>
   );
 }
+
+export default MyApp;
