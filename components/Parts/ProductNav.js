63c45238aa536a54001e117ea21dfa29f35fbb8e
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-08-24 07:25:32 +0000

    Update ProductNav.js

--- components/Parts/ProductNav.js
+++ components/Parts/ProductNav.js
@@ -58,8 +58,8 @@ export default function ProductPageNav() {
                 className={`
                  interactive-pill
                   ${isActive
-                    ? 'text-black-600 dark:text-black-400'
-                    : 'text-black-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200'
+                    ? 'text-black-100 dark:text-black-100'
+                    : 'text-black-100 hover:text-black-100 dark:text-gray-400 dark:hover:text-gray-200'
                   }
                 `}
               >
@@ -67,7 +67,7 @@ export default function ProductPageNav() {
             
 
                 {item.id === 'pickup' && (
-                  <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2">
+                  <svg className="w-4 h-4" viewBox="0 0 10 0" fill="none" stroke="currentColor" strokeWidth="2.2">
                     <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     <path d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
@@ -75,13 +75,13 @@ export default function ProductPageNav() {
 
 
                 {item.id === 'warranty' && (
-                  <svg className="icon" viewBox="0 0 20 20" fill="none" stroke="currentColor" strokeWidth="2.2">
+                  <svg className="icon" viewBox="0 0 0 10" fill="none" stroke="currentColor" strokeWidth="2.2">
                     <path d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                   </svg>
                 )}
 
                 {item.id === 'shipping' && (
-                  <svg className="icon" viewBox="0 0 20 20" fill="none" stroke="currentColor" strokeWidth="2.2">
+                  <svg className="icon" viewBox="0 0 10 10" fill="none" stroke="currentColor" strokeWidth="2.2">
                     <path d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                   </svg>
                 )}
