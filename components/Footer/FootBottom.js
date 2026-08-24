ae085497fbffe28df2e7c448b11d39428a1907f2
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-08-24 16:05:04 +0000

    Update FootBottom.js

--- components/Footer/FootBottom.js
+++ components/Footer/FootBottom.js
@@ -3,31 +3,55 @@
 import Link from "next/link";
 function FootBottom() {
   return (
-    <div className="ibrid-box">
+   <h6 className="text-align-center">Accepted Payment Methods</h6>
+      
       <div className="payment-container">
-        <ul className="list-inline">
-          <li>
-            {" "}
-            <Link href={"/privacy"} className="footer-tag">
-              {" "}
-              Privacy
-            </Link>{" "}
-          </li>
-          |
-          <li>
-            {" "}
-            <Link href={"/terms"} className="footer-tag">
-              {" "}
-              Terms
-            </Link>
-          </li>
-        </ul>
+        <Image
+          src={"/telecel.JPG"}
+          height={20}
+          width={25}
+          alt="momo logo"
+          className="footer-tag"
+        />
+        <Image
+          src={"/momo.jpg"}
+          height={20}
+          width={25}
+          alt="momo logo"
+          className="footer-tag"
+        />
+
+        <Image
+          src={"/Visa.png"}
+          height={20}
+          width={25}
+          alt="momo logo"
+          className="footer-tag"
+        />
+        <Image
+          src={"/Airtel-Money-logo.jpg"}
+           height={20}
+          width={25}
+          alt="momo logo"
+          className="footer-tag"
+        />
       </div>
-      <em>
-        <br /> <h6 className="footer-tag">Powered by Paystack</h6>
-      </em>{" "}
-      <br />
-    </div>
+        <Image
+          src={"View recent photos.png"}
+          height={30}
+          width={130}
+          alt="momo logo"
+          className="signature"
+        />
+      
+   
+      {/* Copyright */}
+      <div>
+        <p className="text-align-center">
+          © {new Date().getFullYear()} Coast Republic Store. All rights
+          reserved.
+        </p>
+      </div>
   );
 }
 export default FootBottom;
