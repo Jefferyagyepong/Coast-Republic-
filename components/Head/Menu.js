b9689bb825b743b668be017258799bc06813989d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-08-25 15:22:41 +0000

    Update Menu.js

--- components/Head/Menu.js
+++ components/Head/Menu.js
@@ -26,6 +26,7 @@ const HamburgerMenu = () => {
         <li><Link className="nav-menu-link" href="/contact">CONTACT US</Link></li>
  <li><Link className="nav-menu-link" href="/about">ABOUT US</Link></li>
  <li><Link className="nav-menu-link" href="/return">REFUNDS</Link></li>
+ <li><Link className="nav-menu-link" href="/terms">TERMS AND CONDITIONS</Link></li>
 
 
           <li> <div className="footer-bottom">
