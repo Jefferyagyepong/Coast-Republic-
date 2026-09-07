c4ddd0ffb210291dd5e3f22a1afd127292d6d8f8
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-07 23:35:45 +0000

    PostgreSQL connection set up

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,3 +5,4 @@ import "./.next/types/root-params.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
+DATABASE_URL="YOUR_USERNAME@localhost:5432/coast_republic"
