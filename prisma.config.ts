72632e6062a808a3846ebf5a55bbdc924e12833d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-08 16:38:19 +0000

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,19 +1,6 @@
-import path from 'path'
-import type { PrismaConfig } from 'prisma'
+import path from "path";
+import type { PrismaConfig } from "prisma";
 
 export default {
-  earlyAccess: true,
-  schema: path.join('prisma', 'schema.prisma'),
-  migrate: {
-    adapter: async () => {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      const { Pool } = await import('pg')
-
-      const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-      })
-
-      return new PrismaPg(pool)
-    },
-  },
-} satisfies PrismaConfig
+  schema: path.join("prisma", "schema.prisma"),
+} satisfies PrismaConfig;
