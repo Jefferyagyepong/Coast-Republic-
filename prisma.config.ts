606e9c0afa671dbf5ffd75468dffee36c08ff488
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-09 08:41:29 +0000

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,9 @@
+// prisma.config.ts
+import "dotenv/config";
 import path from "path";
 import type { PrismaConfig } from "prisma";
 
 export default {
   schema: path.join("prisma", "schema.prisma"),
 } satisfies PrismaConfig;
+
