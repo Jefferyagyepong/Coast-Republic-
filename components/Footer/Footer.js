9d0ca63f6fa69bf546bfba4aa7ed2a81666f9794
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-10 22:14:42 +0000

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -73,7 +73,7 @@ const Footer = () => {
       <Image
         src={"/crlogo2.png"} // ← confirm this file exists in /public, or swap in the real filename
         height={30}
-        width={130}
+        width={50}
         alt="logo"
         className="signature"
       />
