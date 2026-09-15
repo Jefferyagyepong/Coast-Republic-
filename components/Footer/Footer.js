a12c4ad14c08797dcd8dc662ef640ae96fbcd521
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-15 10:58:09 +0000

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -31,9 +31,9 @@ const Footer = () => {
           <div className="footer-brand">
             <Link href="/" className="footer-logo-link">
               <Image
-                src="/IMG_5658.jpeg"
-                width={120}
-                height={45}
+                src="/IMG_5665.jpeg"
+                width={12}
+                height={20}
                 alt="Your store logo"
                 className="footer-logo"
               />
