19f6ac6687d31f80ca05ae2d554e148f4e6a7d66
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-21 18:44:00 +0000

    Update index.js

--- pages/products/index.js
+++ pages/products/index.js
@@ -120,18 +120,22 @@ const ProductsPage = ({ products }) => {
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="google-site-verification" content="HIhs3rvT7a6WD274_Txl6lfu3opycY_McRAFvT2-oBw" />
 
-        {/* Open Graph */}
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Coast Collective | Clothing & Shoes" />
+  
+      
+         {/* Open Graph */}
+        <link rel="canonical" href="https://www.coast-collective.com/" />
+        <meta property="og:url" content="https://www.coast-collective.com/" />
+       
+         <meta property="og:type" content="Online store" />
+     <meta property="og:title" content="Coast Collective | Clothing & Shoes" />
         <meta
           property="og:description"
           content="Ghana's home for quality T-shirts, sneakers, jeans, and street-ready essentials."
         />
-        <meta property="og:url" content="https://www.coast-collective.com/" />
-        <meta property="og:site_name" content="Coast Collective" />
+        
+     
         <meta property="og:image" content="https://www.coast-collective.com/crlogo.svg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
