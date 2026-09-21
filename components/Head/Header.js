ab126dd31eb9c8c136ce45066ea1f2c275447a06
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-21 08:33:28 +0000

    Update Header.js

--- components/Head/Header.js
+++ components/Head/Header.js
@@ -50,7 +50,7 @@ export default function Header() {
         <Link href="/">
       
               <Image
-                src="/crlogo2.png"
+                src="/crlogo.svg"
                 width={28}
                 height={15}
                 alt="Your store logo"
