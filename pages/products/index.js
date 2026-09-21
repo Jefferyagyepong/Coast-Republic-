19ad9183ac01e194915a099a087950db2ea9b283
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-21 20:27:48 +0000

    Update index.js

--- pages/products/index.js
+++ pages/products/index.js
@@ -144,7 +144,8 @@ const ProductsPage = ({ products }) => {
           name="twitter:description"
           content="Ghana's home for quality T-shirts, sneakers, jeans, and street-ready essentials."
         />
-        <meta name="twitter:image" content="https://www.coast-collective.com/crlogo.svg" />
+        <meta name="twitter:image" content="https://www.coast-collective.com/IMG_5722.jpeg" />
+        <meta name="twitter:site" content="https://x.com/ccollectivegh?s=11" />
       </Head>
 
       <Header />
