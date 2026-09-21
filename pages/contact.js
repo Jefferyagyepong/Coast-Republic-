f837a2c5af7748546c0113a6422af32b16708dcc
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-21 17:58:18 +0000

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -40,6 +40,14 @@ export default function Contact() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="canonical" href="https://www.coastrepublic.com/contact" />
+    <meta name="keywords" content="ecommerce, t-shirts, Ghana, quality clothing, sneakers, jeans, crew neck, streetwear" />
+        <meta name="author" content="Coast collective Ghana" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <link rel="canonical" href="https://www.coast-collective.com/" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
 
         {/* Open Graph */}
         <meta property="og:type" content="website" />
