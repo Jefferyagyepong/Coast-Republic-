dd01e2c114725c6b75dc849bdf9fad7a03316399
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-22 19:32:43 +0000

    brand maintenance

--- pages/about.js
+++ pages/about.js
@@ -7,15 +7,10 @@ export default function About() {
     <>
       <Head>
         <title>About Us | Coast Collective</title>
-        <meta
-          name="description"
-          content="Learn the story behind Coast Collective — Ghana's home for quality T-shirts, sneakers, and street-ready essentials."
-        />
-        <meta name="keywords" content="ecommerce, t-shirts, Ghana, quality clothing, sneakers, jeans, crew neck, streetwear" />
+        <meta name="description" content="Learn the story behind Coast Collective — Ghana's home for quality T-shirts, sneakers, and street-ready essentials."/>
         <meta name="author" content="Coast collective Ghana" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <link rel="canonical" href="https://www.coast-collective.com/" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
@@ -68,7 +63,7 @@ export default function About() {
               <section className="block">
                 <h6>Our Story</h6>
                 <p>
-                  Coast Collective is a Ghanaian lifestyle and streetwear brand built for a generation that leads with confidence. Inspired by culture, creativity, and everyday ambition, we offer quality fashion essentials that help people express their identity and elevate their style. More than a brand, Coast Republic is a community of dreamers, creators, and trendsetters shaping the future on their own terms. ￼
+                  Coast Collective is a Ghanaian lifestyle and streetwear brand built for a generation that leads with confidence. Inspired by culture, creativity, and everyday ambition, we offer quality fashion essentials that help people express their identity and elevate their style. More than a brand, Coast Collective is a community of dreamers, creators, and trendsetters shaping the future on their own terms. ￼
                   Brand Essence
                   "Born from Culture. Built for Confidence."
                   Brand DNA
