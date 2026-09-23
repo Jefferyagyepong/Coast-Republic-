03864b4b07df7fab6f2099d2fae2f153726f1146
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-23 08:57:49 +0000

    brand

--- pages/return.js
+++ pages/return.js
@@ -98,7 +98,7 @@ export default function Returns() {
                     <h6>4. How to Start a Return</h6>
                     <ol>
                       <li>
-                        Email <a href="mailto:support@coastcollecive.com">support@coastcollective.com</a>{" "}
+                        Email <a href="mailto:customercare@coastcollecive.com">customercare@coastcollective.com</a>{" "}
                         or call <a href="tel:+233244736420">+233 244 736 420</a> with
                         your order number and reason for return.
                       </li>
