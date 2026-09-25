26da2ea11476f10730d6f9a19fc092f07c3c8233
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2026-09-25 09:22:17 +0000

    content updated

--- components/Parts/Thrifts.js
+++ components/Parts/Thrifts.js
@@ -6,7 +6,8 @@ import Image from "next/image";
 function All() {
   return (
     <div className="background-container-women">
-      <h6> Selling Fast</h6>
+
+      <h6> Nike Dunk Low 'Panda'</h6>
       <Link className="btn-scale-women" href={"/products"}>Shop now</Link>
     </div>
   );
